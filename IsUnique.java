45e90501dc4d9d96eec6c4841947216c72358bbe
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-05-07 10:38:50 -0700

    is unique using brute force

--- IsUnique.java
+++ IsUnique.java
@@ -1,12 +1,31 @@
-// This class check to see it a String as all unique characters.
+/**
+ * This class check to see it a String as all unique characters.
+ */
 
-public class IsUnique {
+public class IsUnique {                                               
 
-    // IsUnique using brute force
-    
-    
+    // isUnique using brute force
+    public static boolean isUniqueBruteForce(String str) {
 
-    public static void main(String[] args) {
-
+        // create out loop of str with 2 less than length
+        for (int i = 0; i < str.length() - 1; i++) {
+            // create inner loop of str starting at i + 1
+            for (int j = i + 1; j < str.length(); j++) {
+                // if char in inner loop is equal to char in outer loop return false
+                if (str.charAt(i) == str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        // after loops return true since it's unique
+        return true;
     }
+
+
+    // main method
+    public static void main(String[] args) {
+        String words = "words";
+        System.out.println(isUniqueBruteForce(words));
+    }
+    
 }
