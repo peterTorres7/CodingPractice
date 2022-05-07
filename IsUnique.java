6a3672d9b5d34c91c851ea219f71d353e5fdcd14
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-05-06 21:51:48 -0700

    initial setup

---
+++ IsUnique.java
@@ -0,0 +1,12 @@
+// This class check to see it a String as all unique characters.
+
+public class IsUnique {
+
+    // IsUnique using brute force
+    
+    
+
+    public static void main(String[] args) {
+
+    }
+}
