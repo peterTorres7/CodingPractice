d8f48eb5cace8d2417c0113f5c4ce54c055d7054
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-05-07 18:01:02 -0700

    is unique using sort

--- IsUnique.java
+++ IsUnique.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 /**
- * This class check to see it a String as all unique characters.
+ * This class checks to see it a String has all unique characters.
  */
 
 public class IsUnique {                                               
 
+    // Time = O(n^2)
+    // Space = O(1)
     // isUnique using brute force
     public static boolean isUniqueBruteForce(String str) {
 
@@ -19,13 +23,33 @@ public class IsUnique {
         }
         // after loops return true since it's unique
         return true;
-    }
+    };
+
+    // Time: O(n log(n))                                                                                                 
+    // isUnique using sort
+    public static boolean isUniqueSort(String str) {
+        
+        //initialize a char array using str
+        char[] characters = str.toCharArray(); 
+        // sort char array
+        Arrays.sort(characters);
+
+        // loop through characters until length - 1
+        for (int i = 0; i < characters.length - 1; i++) {
+            // if char at current index is equal to chart at current index plus 1 return false
+            if (characters[i] == characters[i + 1]) {
+                return false;
+            }
+        }
+        //return true after loop since it's unique
+        return true;
+    };
 
 
     // main method
     public static void main(String[] args) {
         String words = "words";
-        System.out.println(isUniqueBruteForce(words));
+        System.out.println(isUniqueSort(words));
     }
     
 }
