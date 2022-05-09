821c653ca1431ea3b10ea5777536d8ec40c99e22
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-05-09 11:42:09 -0700

    is unique using boolean array

--- IsUnique.java
+++ IsUnique.java
@@ -44,12 +44,37 @@ public class IsUnique {
         //return true after loop since it's unique
         return true;
     };
+                                                                                                                                 
+    // Time = O(n)
+    // Space = O(1)                                                    
+    // isUnique using boolean array
+    public static boolean isUniqueBooleanArray(String str) {
+        // if str length is greater than 128 return false since at least one char is ascii is repeated
+        if (str.length() > 128) {
+            return false;
+        }
 
+        // initialize boolean array to size of 128 for ascii representation
+        boolean[] booleanCharArray = new boolean[128];
+        // loop through str chars
+        for (int i = 0; i < str.length(); i++) {
+            // initialize int char index
+            int charIndex = str.charAt(i);
+            // if boolean array at char index is true return false since char is already seen 
+            if (booleanCharArray[charIndex]) {
+                return false;
+            }
+            // change boolean array at index to true since it hasn't been seen
+            booleanCharArray[charIndex] = true;
+        }
+        // return true after loop since it's unique
+        return true;
+    }
 
     // main method
     public static void main(String[] args) {
         String words = "words";
-        System.out.println(isUniqueSort(words));
+        System.out.println(isUniqueBooleanArray(words));
     }
     
 }
