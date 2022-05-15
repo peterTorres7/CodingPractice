af78af2595e321b42b3be7b7e8a22f95cddf67be
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-05-15 10:39:04 -0700

    updated method

--- URLify.java
+++ URLify.java
@@ -2,47 +2,49 @@
  * This class replaces all spaces in a String with "%20".
  */
 
-public class URLify {
+public class URLify {                                                          
     
     // URLify using brute force
     public static void urlify(String str, int trueLength) {
-        // initialize char array from str
-        char[] strCharacters = str.toCharArray();
-        // initialize space counter
+        // initialze strChars with str as char array
+        char[] strChars = str.toCharArray();
+        // initilize space counter to 0
         int spaceCounter = 0;
-
-        // loop through true length
-        for (int i = 0; i < trueLength; i ++) {
-            // if char array at loop index is a space
-            if (strCharacters[i] == ' ') {
-                // increace space counter by 1
+        // loop through strChars until trueLength - 1
+        for (int i = 0; i < trueLength; i++) {
+            // if char is a space
+            if (strChars[i] == ' ') {
+                // increase space counter by 1
                 spaceCounter++;
             }
         }
 
-        // initialze end index to trueLength plus space counter times 2
-        int endIndex = trueLength + spaceCounter * 2;
-        // loop through in reverse starting at trueLength - 1
+        // initialize lastCharCount to trueLength + space counter * 2 
+        int lastCharCount = trueLength + spaceCounter * 2;
+        // loop backwards through strChars starting at trueLength - 1 
         for (int i = trueLength - 1; i >= 0; i--) {
-            // if char array at loop index is a space
-            if (strCharacters[i] == ' ') {
-                // set char array at end index - 1 to '0'
-                strCharacters[endIndex - 1] = '0';
-                // set char array at end index - 2 to '2'
-                strCharacters[endIndex - 2] = '2';
-                // set char array at end index - 3 to '%'
-                strCharacters[endIndex - 3] = '%';
-                // decrease end index by 3
-                endIndex -= 3;
-            } else {    // else it's a char
-                // set char array at end index - 1 to char array at loop index
-                strCharacters[endIndex - 1] = strCharacters[i];
-                // decrease end index by 1
-                endIndex--;
+            // if char at current loop index is a space
+            if (strChars[i] == ' ') {
+                // set strChars at lastCharCount - 1 to '0'
+                strChars[lastCharCount - 1] = '0';
+                // set strChars at lastCharCount - 2 to '2'
+                strChars[lastCharCount - 2] = '2';
+                // set strChars at lastCharCount - 3 to '%'
+                strChars[lastCharCount - 3] = '%';
+                // decrease lastCharCount by 3
+                lastCharCount -= 3;
+            // else it's a char
+            } else {
+                // set strChars at lastCharCount - 1 to strChars at current loop index
+                strChars[lastCharCount - 1] = strChars[i];
+                // decrease lastcharCount by 1
+                lastCharCount--;
             }
-        }   
+        }
+
         // testing
-        System.out.println(String.valueOf(strCharacters));
+        System.out.println(String.valueOf(strChars));
+
     }
 
     // main method
