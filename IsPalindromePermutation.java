0cef4c82a8a862d2e188aa7628587a23eb6e4f60
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-05-17 16:06:26 -0700

    redid is palindrome permutation using int array

--- IsPalindromePermutation.java
+++ IsPalindromePermutation.java
@@ -1,47 +1,43 @@
+import java.util.Arrays;
+
 /**
  * This class checks to see if given string is a permutation of a palindrome."
  */
 
 public class IsPalindromePermutation {
-    
-    //  "Tact Coa"      
-    //   i                                                                                  
-    //  [a, b, c, d, e, f, ... , x, y, z]
-    //   1     1              2                       
-    //                                               
-    //                                               
-
-    // isPalindromePermutation using char array
+             
+    // Time = O(n)
+    // isPalindromePermutation using int array
     public static boolean isPalindromePermutationCharArray (String str) {
-        // initialze array of size 128 to hold ascii chars
-        char[] charArray = new char[128];
+        // initize char array to hold char count
+        int[] charCount = new int[128];
         // loop through str
         for (int i = 0; i < str.length(); i++) {
-            // initialze int charValue to each char in loop
+            // initialize int char value to str char at current index
             int charValue = str.charAt(i);
-            // in charArray at charValue index increase count by 1
-            charArray[charValue]++;
+            // in char array at char value index increase by 1
+            charCount[charValue]++; 
         }
+            System.out.println(Arrays.toString(charCount));
+        // initialize boolean to hold if odd seen to false
+        boolean oddCountSeen = false;
+        // loop through charCountsArray
+        for (int i = 0; i < charCount.length; i++) {
 
-        // initizalize boolean seenOdd to false
-        boolean seenOdd = false;
-        // loop through charArray
-        for (int i = 0; i < charArray.length; i++) {
-            // if char count in charArray is odd
-            if (charArray[i] % 2 != 0) {
-                // if seenOdd is true
-                if (seenOdd) {
-                    // return false since only one add char can appear
+            // if char count is odd
+            if (charCount[i] % 2 != 0) {
+                // if odd seen is true
+                if (oddCountSeen) {
+                    // return false since not a palindrome permutation
                     return false;
-                // else set seenOdd to true
-                } else {
-                    seenOdd = true;
                 }
+                // set odd seen to true
+                oddCountSeen = true;
             }
         }
-        // return true after loop since it's palindrome permutation
+        // return true after loop since it is palindrome permutation
         return true;
-    };
+    };     
 
     // main method
     public static void main(String[] args) {
