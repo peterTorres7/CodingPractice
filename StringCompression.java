61cbcbb86fe918285a0fb6dd7e748cd3fede8619
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-05-27 16:06:31 -0700

    string compression using StringBuilder

--- StringCompression.java
+++ StringCompression.java
@@ -5,7 +5,7 @@
 public class StringCompression {                             
 
     // Time: O(n^2)
-    // stringCompression using String as storage
+    // string compression using String as storage
     public static String stringCompressionString (String str) {
         // initialize compressedString to empty string
         String compressedString = "";
@@ -28,9 +28,34 @@ public class StringCompression {
         // return smallest between str length and compressedString length
         return compressedString.length() < str.length() ? compressedString : str;
     }
-  
-    // main
+                  
+    // Time: O(n)
+    // string compression using StringBuilder as storage
+    public static String stringCompressionStringBuilder(String str) {
+        // initialize compressedString as new StringBuilder
+        StringBuilder compressedString = new StringBuilder();
+        // initialize charCount to 0
+        int charCount = 0;
+        // loop through str
+        for (int i = 0; i < str. length(); i++) {
+            // increase charCount by 1
+            charCount++;
+            // if i + 1 is greater than or equal to str lenght OR str char at current index is not equal to str char at current index plus 1
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                // to compressedString add str char at current index
+                compressedString.append(str.charAt(i));
+                // to compressedString add charCount
+                compressedString.append(charCount);
+                // reset charCount to 0 
+                charCount = 0;
+            }
+        }
+        // return shortest string between str and compressedString
+        return compressedString.length() < str.length() ? compressedString.toString() : str;
+    }
+
+    // main method
     public static void main(String[] args) {
-        System.out.println(stringCompressionString("aabcccccaaa"));
+        System.out.println(stringCompressionStringBuilder("aabcccccaaa"));
     }
 }
