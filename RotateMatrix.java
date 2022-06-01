d2b08171f844d89361b4edb2c1708b59c4df1955
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-06-01 11:01:04 -0700

    created rotate matrix file

---
+++ RotateMatrix.java
@@ -0,0 +1,25 @@
+/**
+ * This class rotates NxN matrix by 90 degrees.
+ */
+
+public class RotateMatrix {
+    //                         
+    //                         
+    //                         
+    //                         
+    //                         
+    //                         
+
+    // rotates matrix in place
+    public static void rotateMatrix(int[][] matrix) {
+        // if matrix length is equal to 0 OR matrix length not equal to matrix return false
+        // initialize matrixLength to matrix lenth
+        // loop through have the matrix
+        // initialize first as layer
+        // initialize last to matrixLength minus 1 minus layer
+        // loop from first to last
+        // initialize offset to i minus first
+        // initialzie top to hold top
+        
+    }
+}
