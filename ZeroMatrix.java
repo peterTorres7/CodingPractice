54030b6d30c9eb7196802428350e6b6dcce99837
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-06-02 10:06:53 -0700

    created zero matrix file

---
+++ ZeroMatrix.java
@@ -0,0 +1,18 @@
+/**
+ * This class checks an MxN matrix and if an element is 0,
+ * its entire row and column are set to 0.
+ */
+
+public class ZeroMatrix {
+    //  3 5 1 0 7                    
+    //  0 1 9 1 0                        
+    //  4 4 1 7 5                        
+    //                          
+    //                          
+    //                          
+
+    // zero matrix using boolean arrays
+    public static void zeroMatrixBooleanArray (int[][] matrix) {
+
+    }
+}
