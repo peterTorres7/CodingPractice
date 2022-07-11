4235c63844a5b207a39c60dc42121955a85f2acf
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-11 10:07:14 -0700

    remove duplicates from sorted array using two pointers

--- leetcode/RemoveDuplicatesFromSortedArray.java
+++ leetcode/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,39 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class RemoveDuplicatesFromSortedArray {
-    
+
+    // Time: O(n)
+    // Space: O(1)
+    // remove duplicates from sorted array in place using two pointers
+    public static int removeDuplicatesFromSortedArray(int[] nums) {
+        // initialize int start index
+        int startIndex = 0;
+        // initialize int end index
+        int endIndex = 1;
+        // loop through nums
+        while (endIndex < nums.length) {
+            // if nums at start index is equal to nums at end index
+            if (nums[startIndex] == nums[endIndex]) {
+                // increase end index by 1
+                endIndex++;
+            } else {
+                // else increase start index by 1
+                startIndex++;
+                // set nums at start index equal to nums at end index
+                nums[startIndex] = nums[endIndex];
+            }
+        }
+        // print array
+        System.out.println(Arrays.toString(nums));
+        // after loop return start index plus 1
+        return startIndex + 1;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicatesFromSortedArray(nums));
+    }
 }
