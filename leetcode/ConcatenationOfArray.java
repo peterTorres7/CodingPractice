8cbdcc2bfa612f51412e7478e476d5e223358b45
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-12 20:12:44 -0700

    concatenation of array using one pointer

---
+++ leetcode/ConcatenationOfArray.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * This class builds an array ans that is the concatenation of two nums arrays."
+ */
+
+public class ConcatenationOfArray {
+    
+    // Time: O(n)
+    // Space: O(1)
+    // contatenates twm nums arrays using one pointer
+    public static int[] concatenationOfArray(int[] nums) {
+        // create int array ans to new int array of nums length times 2
+        int[] ans = new int[nums.length * 2];
+        // create int numsPointer to 0
+        int numsPointer = 0;
+        // while numsPointer is less than nums length
+        while (numsPointer < nums.length) {
+            // set ans at numsPointer to nums at numsPointer
+            ans[numsPointer] = nums[numsPointer];
+            // set ans at nums length plus numsPointer to nums at numsPointer
+            ans[nums.length + numsPointer] = nums[numsPointer];
+            // increase numsPointer
+            numsPointer++;
+        }
+        // after loop return ans
+        return ans;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        int[] nums = {1,3,2,1};
+        System.out.println(Arrays.toString(concatenationOfArray(nums)));
+    }
+}
