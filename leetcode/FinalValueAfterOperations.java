e6d04269cb238d0a0fa750e863f9e9fd2ed93f97
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-14 10:20:40 -0700

    final value after operations using for each loop

---
+++ leetcode/FinalValueAfterOperations.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class FinalValueAfterOperations {
+    
+    // Time: O()
+    // final value of variable after performing operations 
+    public static int finalValueAfterOperations(String[] operations) {
+        // initialize x to 0
+        int x = 0;
+        // loop through operations
+        for(String operation : operations) {
+            // if operations at current index is equal to "++X" OR "X++" 
+            if(operation.equals("++X") || operation.equals("X++")) {
+                // increase x by 1
+                x++;
+            } else {
+                // else decrease x by 1
+                x--;
+            }
+        }
+        // after loop return x
+        return x;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        String[] operations = {"--X","X++","X++"};
+        System.out.println(finalValueAfterOperations(operations));
+    }
+
+}
