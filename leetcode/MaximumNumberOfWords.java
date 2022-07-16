f50ef0623181a99e56849380debb86dd335c1067
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-16 16:06:58 -0700

    maximum number of words using for loop

--- leetcode/MaximumNumberOfWords.java
+++ leetcode/MaximumNumberOfWords.java
@@ -7,7 +7,29 @@ package leetcode;
 
 public class MaximumNumberOfWords {
     
-    // Time: O()
-    // mazimum number of words using
+    // Time: O(n*m)
+    // maximum number of words using for loop
+    public static int maximumNumberOfWords(String[] sentences) {
+        // initialize maxWords to 0
+        int maxWords = 0;
+        // for each sentence in sentences
+        for (String sentence : sentences) {
+            // initialize splitSentence to sentence split by " "
+            String[] splitSentence = sentence.split("\\s+");
+            // if splitSentence length is greater than maxWords
+            if (splitSentence.length > maxWords) {
+                // set maxWords to splitSentence length
+                maxWords = splitSentence.length;
+            }
+        }
+        // after loop return maxWords
+        return maxWords;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        String[] sentences = {"this is in Java", "I like solving all these puzzles", "hello"};
+        System.out.println(maximumNumberOfWords(sentences));
+    }
     
 }
