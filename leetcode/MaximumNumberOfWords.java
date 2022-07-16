a163c86b3b2063efef8926cd982e5ddfce0e3a35
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-15 20:58:49 -0700

    created maximum number of words file

---
+++ leetcode/MaximumNumberOfWords.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * This class returns the maximum number of words that appear in a single sentence
+ * from a given array."
+ */
+
+public class MaximumNumberOfWords {
+    
+    // Time: O()
+    // mazimum number of words using
+    
+}
