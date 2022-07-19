5ac6ce2a8b8ba2eda458f3172bbc26d9ba4fcd93
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-19 16:16:51 -0700

    created number of good pairs file

---
+++ leetcode/NumberOfGoodPairs.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+    Given an array of integers nums, return the number of good pairs.
+
+    A pair (i, j) is called good if nums[i] == nums[j] and i < j.   
+
+    Example 1:
+
+    Input: nums = [1,2,3,1,1,3]
+    Output: 4
+    Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+ */
+
+public class NumberOfGoodPairs {
+    
+    // Time: O()
+    // number of good pairs brute force
+    public static int numberOfGoodPairsBruteForce (int[] nums) {
+        
+    }
+}
