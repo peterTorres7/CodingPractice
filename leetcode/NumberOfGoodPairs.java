6e79a41063fa30a05dcef522882635a95d84fc28
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-19 23:36:08 -0700

    number of good pairs using sort

--- leetcode/NumberOfGoodPairs.java
+++ leetcode/NumberOfGoodPairs.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
     Given an array of integers nums, return the number of good pairs.
 
@@ -34,9 +36,47 @@ public class NumberOfGoodPairs {
         return goodPairs;
     }
 
+    // Time: O(n logn)
+    // number of good pairs using sort
+    public static int numberOfGoodPairsSort (int[] nums) {
+        // sort nums
+        Arrays.sort(nums);
+
+        // initialize goodPairs to 0
+        int goodPairs = 0;
+        // initialize frequencyCount to 1
+        int frequencyCount = 1;
+        // initialize numsPointer to 1
+        int numsPointer = 1;
+        
+        // while numsPointer is less than nums length
+        while (numsPointer < nums.length) {
+            // if nums at numsPointer is equal to nums at numsPointer minus 1
+            if (nums[numsPointer] == nums[numsPointer - 1]) {
+                // increase frequencyCount by 1
+                frequencyCount++;
+                // increase numsPointer by 1
+                numsPointer++;
+            }
+            // if numsPointer is equal to nums lengh OR nums at numsPointer is not equal to nums at numsPointer minus 1
+            if (numsPointer == nums.length || nums[numsPointer] != nums[numsPointer - 1]) {
+                // to goodPairs at ((frequencyCount) * (frequencyCount - 1)) / 2
+                goodPairs += ((frequencyCount) * (frequencyCount - 1)) / 2;
+                // set frequencyCount to 1
+                frequencyCount = 1;
+                // increase numsPointer by 
+                numsPointer++;
+            }            
+        }
+
+        // after loop return goodPairs
+        return goodPairs;
+    }
+
+
     // main method
     public static void main(String[] args) {
         int[] nums = {1,2,3,1,1,3};
-        System.out.println(numberOfGoodPairsBruteForce(nums));
+        System.out.println(numberOfGoodPairsSort(nums));
     }
 }
