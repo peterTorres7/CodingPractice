fad9d75849ad6b2f5a9fd985a501ab9646d26013
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-22 13:29:47 -0700

    kids with the greatest number of candies using for loop

--- leetcode/KidsWithTheGreatestNumberOfCandies.java
+++ leetcode/KidsWithTheGreatestNumberOfCandies.java
@@ -1,13 +1,18 @@
 package leetcode;
- 
-/**
-    There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
 
-    Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+    There are n kids with candies. You are given an integer array candies, where each candies[i] 
+    represents the number of candies the ith kid has, and an integer extraCandies, 
+    denoting the number of extra candies that you have.
+
+    Return a boolean array result of length n, where result[i] is true if, 
+    after giving the ith kid all the extraCandies, they will have 
+    the greatest number of candies among all the kids, or false otherwise.
 
     Note that multiple kids can have the greatest number of candies.
-
-    
 
     Example 1:
 
@@ -24,7 +29,43 @@ package leetcode;
 
 public class KidsWithTheGreatestNumberOfCandies {
     
-    // Time: O()
-    // kids with greatest number of candies using brute force
+    // Time: O(n)
+    // kids with greatest number of candies using for loop
+    public static List<Boolean> kidsWithTheGreatesNumberOfCandies (int[] candies, int extraCandies) {
+        // initialize maxCandies to 0
+        int maxCandies = 0;
+        // for each candie in candies
+        for (int candie : candies) {
+            // if candie is greater than or equal to maxCandies
+            if (candie >= maxCandies) {
+                // set maxCandies to candie
+                maxCandies = candie;
+            }
+        }
+
+        // initialize candiesList to new array list
+        List<Boolean> candiesList = new ArrayList<>();
+        // for each candie in candies
+        for (int candie : candies) {
+            // if candie plus extraCandies is greater than or equal to maxCandies
+            if (candie + extraCandies >= maxCandies) {
+                // to candiesList add true
+                candiesList.add(true);
+            } else {
+                // else to candiesList add false
+                candiesList.add(false);
+            }
+        }
+
+        // return candiesList
+        return candiesList;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        int extraCandies = 3;
+        System.out.println(kidsWithTheGreatesNumberOfCandies(candies, extraCandies));
+    }
     
 }
