9fdf70c2cdf3b6e1f4606c408071d8d740577abd
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-07-24 22:22:13 -0700

    created smaller numbers than current file

---
+++ leetcode/SmallerNumbersThanCurrent.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+    Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+    Return the answer in an array.
+
+    
+
+    Example 1:
+
+    Input: nums = [8,1,2,2,3]
+    Output: [4,0,1,1,3]
+ */
+
+public class SmallerNumbersThanCurrent {
+
+    // Time: O()
+    // smaller numbers than current using brute force
+}
