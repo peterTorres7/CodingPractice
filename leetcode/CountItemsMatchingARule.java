06d0080f3bf06268d7d102626b089ade0d9b26da
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-03 20:04:15 -0700

    created count items matching a rule file

---
+++ leetcode/CountItemsMatchingARule.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+
+    You are given an array items, where each items[i] = [typei, colori, namei] describes the type, 
+    color, and name of the ith item. You are also given a rule represented by two strings, 
+    ruleKey and ruleValue.
+
+    The ith item is said to match the rule if one of the following is true:
+
+        ruleKey == "type" and ruleValue == typei.
+        ruleKey == "color" and ruleValue == colori.
+        ruleKey == "name" and ruleValue == namei.
+
+    Return the number of items that match the given rule. 
+
+    Example 1:
+
+    Input: items = [["phone","blue","pixel"],
+                    ["computer","silver","phone"],
+                    ["phone","gold","iphone"]], 
+                    
+                    ruleKey = "type", ruleValue = "phone"
+    Output: 2
+
+ */
+public class CountItemsMatchingARule {
+    
+    // Time: O()
+    // count items matching a rule using for loop
+}
