5a0dc08ffd7c83ed80c75ae5eac45c659976778e
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-04 22:40:17 -0700

    created sum of all odd length subarrays file

---
+++ leetcode/SumOfAllOddLengthSubarrays.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+
+    Given an array of positive integers arr, 
+    return the sum of all possible odd-length subarrays of arr.
+
+    A subarray is a contiguous subsequence of the array. 
+
+    Example 1:
+
+    Input: arr = [1,4,2,5,3]
+    Output: 58
+
+ */
+
+public class SumOfAllOddLengthSubarrays {
+    
+    // Time O()
+    // sum of all odd length subarrays using brute force
+    
+}
