a4f9f0f61e61ab081504f98340effb898e9f0f0d
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-06 22:04:55 -0700

    sum of all odd length subarrays using brute force

--- leetcode/SumOfAllOddLengthSubarrays.java
+++ leetcode/SumOfAllOddLengthSubarrays.java
@@ -16,17 +16,36 @@ package leetcode;
 
 public class SumOfAllOddLengthSubarrays {
     
-    // Time O()
+    // Time O(n*m)
     // sum of all odd length subarrays using brute force
     public static int sumOfAllOddLengthSubarrays (int[] arr) {
         // init sum to 0
+        int sum = 0;
+        // init arraySize to 1
+        int arraySize = 1;
 
-        // for each num in arr lenth increasing by 2 
-        // for each num in arr length
-        // if i plus j is less than array length
-        // for 0 to equal to i
-        // to sum add array at j + k
+        // while arraySize is less than or equal to arr length
+        while (arraySize <= arr.length) {
+            // for each num i starting at 0, if i plus arraySize is less than or equal to  arr length
+            for (int i = 0; i + arraySize <= arr.length; i++) {
+                // for each num j starting at i, if j is less than i plus arraySize
+                for (int j = i; j < i + arraySize; j++) {
+                    // to sum add arr at j 
+                    sum += arr[j];
+                }
+            }
+            // increase arraySize by 2
+            arraySize += 2;
+        }
 
         // return sum
+        return sum;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,4,2,5,3};
+        System.out.println(sumOfAllOddLengthSubarrays(arr));
+        
     }
 }
