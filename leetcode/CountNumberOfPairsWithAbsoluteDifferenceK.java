2e8018b0770456bb4265edfd443339b90e9fc311
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-14 11:01:44 -0700

    count number of pairs with absolute difference using brute force

---
+++ leetcode/CountNumberOfPairsWithAbsoluteDifferenceK.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+/**
+
+    Given an integer array nums and an integer k, 
+    return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+    
+
+    Example 1:
+
+    Input: nums = [1,2,2,1], k = 1
+    Output: 4
+
+ */
+
+public class CountNumberOfPairsWithAbsoluteDifferenceK {
+    
+    // Time: O(n^2)
+    // count number of pairs with absolute difference k using brute force
+    public static int countNumberOfPairsWithAbsoluteDifferenceKBruteForce (int[] nums, int k) {
+        // init ans to 0
+        int ans = 0;
+
+        // for each num i in nums length
+        for (int i = 0; i < nums.length; i++) {
+            // for each num j equal i + 1 in nums length
+            for (int j = i + 1; j < nums.length; j++) {
+                // if math absolute of nums at i minus nums at j equal diff
+                if (Math.abs(nums[i] - nums[j]) == k) {
+                    // increase ans by 1
+                    ans++;
+                }
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,1};
+        int k = 1;
+        System.out.println(countNumberOfPairsWithAbsoluteDifferenceKBruteForce(nums, k));
+    }
+}
