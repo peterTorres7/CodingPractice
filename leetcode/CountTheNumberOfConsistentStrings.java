737ae1b14bab1f4e882aaf49b4725c9c049606bc
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-19 21:14:31 -0700

    created count the number of consistent strings file

---
+++ leetcode/CountTheNumberOfConsistentStrings.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+
+    You are given a string allowed consisting of distinct characters and an array of strings words. 
+    A string is consistent if all characters in the string appear in the string allowed.
+
+    Return the number of consistent strings in the array words.
+    
+
+    Example 1:
+
+    Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+    Output: 2
+
+ */
+
+public class CountTheNumberOfConsistentStrings {
+    
+    // Time: O()
+    // count the number of consistent strings
+    
+}
