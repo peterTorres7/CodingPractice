4bf3abe6831773cc486daaa25888cb65d97e20e4
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-20 23:08:50 -0700

    created to lower case file

---
+++ leetcode/ToLowerCase.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+
+    Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+
+    Example 1:
+
+    Input: s = "Hello"
+    Output: "hello"
+
+ */
+
+public class ToLowerCase {
+
+    // Time: O(1)
+    // to lower case
+
+}
