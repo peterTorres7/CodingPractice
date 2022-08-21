3e21a2afb0ad233d5c1569f6debc494002c16ca4
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-20 20:18:51 -0700

    created truncate sentence file and sudo code

---
+++ leetcode/TruncateSentence.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+
+    You are given a sentence s​​​​​​ and an integer k​​​​​​. 
+    You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+    
+
+    Example 1:
+
+    Input: s = "Hello how are you Contestant", k = 4
+    Output: "Hello how are you"
+
+*/
+
+public class TruncateSentence {
+    
+    // Time: O()
+    // truncate sentence
+    public static String truncateSentence (String s, int k) {
+        // init Stringbuilder sb
+        // init spaceCount to 0
+
+        // for each char in s
+        // if char equals ' '
+        // increase spaceCount by 1
+        // if spaceCount equals k
+        // return sb to string
+        // to sb append char
+
+    }
+}
