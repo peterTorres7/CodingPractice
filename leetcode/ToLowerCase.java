59c643e6f803a016102d83885f7b290239d73e19
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-21 20:43:21 -0700

    to lower case using for loop

--- leetcode/ToLowerCase.java
+++ leetcode/ToLowerCase.java
@@ -15,14 +15,36 @@ package leetcode;
 public class ToLowerCase {
 
     // Time: O(1)
-    // to lower case
+    // to lower case using toLowerCase method
     public static String toLowerCase (String s) {
         return s.toLowerCase();
+    }
+
+    // Time; O(n)
+    // to lower case using loop
+    public static String toLowerCaseLoop (String s) {
+        // init stringbuilder sb
+        StringBuilder sb = new StringBuilder();
+
+        // for each char in s
+        for (char c : s.toCharArray()) {
+            // if char is upper case
+            if (Character.isUpperCase(c)) {
+                // to sb append char to lower case
+                sb.append(Character.toLowerCase(c));
+            } else {
+                // else to sb append char
+                sb.append(c);
+            }
+        }
+
+        // return sb to string
+        return sb.toString();
     }
 
     // main 
     public static void main(String[] args) {
         String s = "Hello";       
-        System.out.println(toLowerCase(s));
+        System.out.println(toLowerCaseLoop(s));
     }
 }
