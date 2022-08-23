16d3cbbb58f0d5c7537f4a6d1fa087a128d579bd
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-23 14:46:13 -0700

    created count equal and divisible pairs in an array file

---
+++ leetcode/CountEqualAndDivisiblePairsInAnArray.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+
+/**
+
+    Given a 0-indexed integer array nums of length n and an integer k, 
+    return the number of pairs (i, j) where 0 <= i < j < n, 
+    such that nums[i] == nums[j] and (i * j) is divisible by k.
+
+
+    Example 1:
+
+    Input: nums = [3,1,2,2,2,1,3], k = 2
+    Output: 4
+
+ */
+public class CountEqualAndDivisiblePairsInAnArray {
+    
+    // Time: O()
+    // count equal and divisible pairs in an array using brute force
+    
+}
