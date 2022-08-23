8a3537ca41ad26b307856788483c044599ff7b22
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-23 15:03:06 -0700

    count equal and divisible pairs in an array using brute force

--- leetcode/CountEqualAndDivisiblePairsInAnArray.java
+++ leetcode/CountEqualAndDivisiblePairsInAnArray.java
@@ -16,7 +16,32 @@ package leetcode;
  */
 public class CountEqualAndDivisiblePairsInAnArray {
     
-    // Time: O()
+    // Time: O(n^2)
     // count equal and divisible pairs in an array using brute force
-    
+    public static int countEqualAndDivisiblePairsInAnArray (int[] nums, int k) {
+        // init pairs to 0
+        int pairs = 0;
+
+        // for each num i in nums
+        for (int i = 0; i < nums.length; i++) {
+            // for each num j in nums
+            for (int j = i + 1; j < nums.length; j++) {
+                // if num i equals num j AND i * j is divisible by k
+                if (nums[i] == nums[j] && i * j % k == 0) {
+                    // increase pairs by 1
+                    pairs++;
+                }
+            }
+        }
+
+        // return pairs
+        return pairs;
+    }
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,2,2,1,3};
+        int k = 2;
+        System.out.println(countEqualAndDivisiblePairsInAnArray(nums, k));
+    }
 }
