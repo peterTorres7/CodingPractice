159c05114f5e2ed15fd44d47036b7b6d67bec1a3
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-24 17:39:58 -0700

    flipping an image using brute force

--- leetcode/FlippingAnImage.java
+++ leetcode/FlippingAnImage.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
 
     Given an n x n binary matrix image, flip the image horizontally, 
@@ -23,7 +25,54 @@ package leetcode;
 
 public class FlippingAnImage {
     
-    // Time: O()
+    // Time: O(n*m)
     // flipping an image using brute force
-    
+    public static int[][] flippingAnImage (int[][] image) {
+        // horizontal flip
+        // for each column in image
+        for (int[] row : image) {
+            // init left to 0
+            int left = 0;
+            // init right to row length - 1
+            int right = row.length - 1;
+            // while left is less than right
+            while (left < right) {
+                // init temp to row at left
+                int temp = row[left];
+                // set row at left to row at right
+                row[left] = row[right];
+                // set row at right to temp
+                row[right] = temp;
+                // increase left by 1
+                left++;
+                // decrease right by 1
+                right--;
+            }
+        }
+
+        // invert
+        // for each row in image
+        for (int i = 0; i < image.length; i++) {
+            // for each column in row 
+            for (int j = 0; j < image[i].length; j++) {
+                // if image at row column equals 0
+                if (image[i][j] == 0) {
+                    // set image at row column to 1
+                    image[i][j] = 1;
+                } else {
+                    // else set image at row column to 0
+                    image[i][j] = 0;
+                }
+            }
+        }
+
+        // return image
+        return image;
+    }
+
+    // main
+    public static void main (String[] args) {
+        int[][] image = {{1,1,0},{1,0,1},{0,0,0}};
+        System.out.println(Arrays.deepToString(flippingAnImage(image)));
+    }
 }
