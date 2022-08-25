dd561a41d35ec48fe2c3c27015b6ec2cecad4059
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-25 13:33:11 -0700

    created count asterisks file

---
+++ leetcode/CountAsterisks.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+
+    You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. 
+    In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
+
+    Return the number of '*' in s, excluding the '*' between each pair of '|'.
+
+    Note that each '|' will belong to exactly one pair.
+
+    
+    Example 1:
+
+    Input: s = "l|*e*et|c**o|*de|"
+    Output: 2
+
+ */
+
+public class CountAsterisks {
+    
+    // Time: O()
+    // count asterisks using brute force
+    
+}
