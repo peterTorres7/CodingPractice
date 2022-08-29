05fb9c2474349f0624ce088b0895fdaaaf976fa0
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-29 09:25:40 -0700

    created check if the sentence is pangram file and template

---
+++ leetcode/CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+
+    A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+    Given a string sentence containing only lowercase English letters, 
+    return true if sentence is a pangram, or false otherwise.
+    
+
+    Example 1:
+
+    Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+    Output: true
+
+ */
+
+public class CheckIfTheSentenceIsPangram {
+    
+    // Time: O(n)
+    // check if the senctence is pangram using map
+    
+}
