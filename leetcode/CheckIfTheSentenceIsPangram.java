02f754ffb6fb23ea620f593d9ef293e9e36a3aa3
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-29 09:41:56 -0700

    check if the sentence is pangram using map

--- leetcode/CheckIfTheSentenceIsPangram.java
+++ leetcode/CheckIfTheSentenceIsPangram.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
 
     A pangram is a sentence where every letter of the English alphabet appears at least once.
@@ -19,5 +22,37 @@ public class CheckIfTheSentenceIsPangram {
     
     // Time: O(n)
     // check if the senctence is pangram using map
-    
+    public static boolean checkIfTheSentenceIsPangramMap (String sentence) {
+        // init ans to true
+        boolean ans = true;
+        // init map of Character, Boolean
+        Map<Character, Boolean> map = new HashMap<>();
+        
+        // for each char 'a' to 'z'
+        for (char c = 'a'; c <= 'z'; c++) {
+            // in map put char, false
+            map.put(c, false);
+        }
+
+        //for each char in sentence
+        for (char c : sentence.toCharArray()) {
+            // in map put char, true
+            map.put(c, true);
+        }
+
+        // if map contains value false
+        if (map.containsValue(false)) {
+            // set ans to false
+            ans = false;
+        }
+
+        // return ans
+        return ans;
+    }
+
+    // main
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        System.out.println(checkIfTheSentenceIsPangramMap(sentence));
+    }
 }
