f6ef7ccfe5e0883aef997e55b73827abc98f2723
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-30 09:50:39 -0700

    number of strings that appear as substrings in word using contains

--- leetcode/NumberOfStringsThatAppearAsSubstringsInWord.java
+++ leetcode/NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -17,7 +17,30 @@ package leetcode;
 
 public class NumberOfStringsThatAppearAsSubstringsInWord {
     
-    // Time: O()
+    // Time: O(n)
     // number of strings that appear as substrings in word using contains
-    
+    public static int numberOfStringsThatAppearAsSubstringsInWordContains (String[] patterns, String word) {
+        // init ans to 0
+        int ans = 0;
+        
+        // for each pattern in patters
+        for (String pattern : patterns) {
+            // if word contains pattern
+            if (word.contains(pattern)) {
+                // increase ans by 1
+                ans++;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+    // main
+    public static void main(String[] args) {
+        String[] patterns = {"a","abc","bc","d"};
+        String word = "abc";
+        System.out.println(numberOfStringsThatAppearAsSubstringsInWordContains(patterns, word));
+    }
 }
