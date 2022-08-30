2c8e4e4753e03b6b8ba6e6afe24fa3341fca1521
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-30 09:44:14 -0700

    created number of strings that appear as substrings in word file and template

---
+++ leetcode/NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+
+    Given an array of strings patterns and a string word, 
+    return the number of strings in patterns that exist as a substring in word.
+
+    A substring is a contiguous sequence of characters within a string.
+
+
+    Example 1:
+
+    Input: patterns = ["a","abc","bc","d"], word = "abc"
+    Output: 3
+
+ */
+
+public class NumberOfStringsThatAppearAsSubstringsInWord {
+    
+    // Time: O()
+    // number of strings that appear as substrings in word using contains
+    
+}
