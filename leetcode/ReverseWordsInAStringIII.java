73c1ef1f4c31fec97090b1677bdce9c113114ab2
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-08-30 21:54:12 -0700

    reverse words in a string III

--- leetcode/ReverseWordsInAStringIII.java
+++ leetcode/ReverseWordsInAStringIII.java
@@ -17,5 +17,47 @@ public class ReverseWordsInAStringIII {
     
     // Time: O()
     // reverse words in a string III 
-    
+    public static String reverseWordsInAStringIII (String s) {
+        // init stringbuilder ans 
+        StringBuilder ans = new StringBuilder();
+        // init string array words to s split by " "
+        String[] words = s.split("\\s");
+
+        // for each word in words
+        for (String word : words) {
+            // init char array wordChars to word to char array
+            char[] wordChars = word.toCharArray();
+            // init left to 0
+            int left = 0;
+            // init right to wordChars length minus 1
+            int right = wordChars.length - 1;
+            // while left is less than right
+            while (left < right) {
+                // init tempChar to wordChars at left
+                char tempChar = wordChars[left];
+                // set wordChars at left to wordChars at right
+                wordChars[left] = wordChars[right];
+                // set wordChars at right to tempChar
+                wordChars[right] = tempChar;
+                // increase left by 1
+                left++;
+                // decrease right by 1
+                right--;
+            }
+            // after while loop init string reversedWord to new string of wordsChars
+            String reversedWord = new String(wordChars);
+            // to ans append reversedWord append " "
+            ans.append(reversedWord).append(" ");
+        }
+
+        // return ans to string trim
+        return ans.toString().trim();
+    }
+
+
+    // main
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        System.out.println(reverseWordsInAStringIII(s));
+    }
 }
