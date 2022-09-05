9f2c00203e52fff232be647bc0bfa092f0cb346a
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-05 15:04:04 -0700

    created decrypt string from alphabet to integer mapping file template

---
+++ leetcode/DecryptStringFromAlphabetToIntegerMapping.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+
+    You are given a string s formed by digits and '#'. 
+    We want to map s to English lowercase characters as follows:
+
+        Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+        Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+
+    Return the string formed after mapping.
+
+    The test cases are generated so that a unique mapping will always exist.
+
+
+    Example 1:
+
+    Input: s = "10#11#12"
+    Output: "jkab"
+
+*/
+
+public class DecryptStringFromAlphabetToIntegerMapping {
+    
+    // Time: O()
+    // decrypt string from alphabet to integer mapping
+    
+}
