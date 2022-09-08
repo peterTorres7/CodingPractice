b95e562197d1ca24f34b9cb7140f24264b21f6a5
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-07 19:22:48 -0700

    created find first palindromic string in the array file template

---
+++ leetcode/FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+
+    Given an array of strings words, return the first palindromic string in the array. 
+    If there is no such string, return an empty string "".
+
+    A string is palindromic if it reads the same forward and backward.
+    
+
+    Example 1:
+
+    Input: words = ["abc","car","ada","racecar","cool"]
+    Output: "ada"
+
+*/
+
+public class FindFirstPalindromicStringInTheArray {
+    
+    // Time: O()
+    // find first palindromic string in the array
+    
+}
