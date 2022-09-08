8876445fa68109f174dbbfdd80a20808b3f074d0
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-08 13:49:41 -0700

    created destination city file and template

---
+++ leetcode/DestinationCity.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+
+    You are given the array paths, where paths[i] = [cityAi, cityBi] 
+    means there exists a direct path going from cityAi to cityBi. 
+    Return the destination city, that is, the city without any path outgoing to another city.
+
+    It is guaranteed that the graph of paths forms a line without any loop, therefore, 
+    there will be exactly one destination city.
+
+    
+    Example 1:
+
+    Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+    Output: "Sao Paulo"
+
+ */
+
+public class DestinationCity {
+    
+    // Time: O(n)
+    // destination city using map
+    
+}
