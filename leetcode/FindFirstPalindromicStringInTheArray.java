629a736414ed287493fbee506873c69c1d2a661c
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-07 19:56:22 -0700

    find first palindromic string in the array using for loop

--- leetcode/FindFirstPalindromicStringInTheArray.java
+++ leetcode/FindFirstPalindromicStringInTheArray.java
@@ -17,7 +17,51 @@ package leetcode;
 
 public class FindFirstPalindromicStringInTheArray {
     
-    // Time: O()
+    // Time: O(n*m)
     // find first palindromic string in the array
-    
+    public static String findFirstPalindromicStringInTheArray (String[] words) {
+        // init string ans to ""
+        String ans = "";
+
+        // for each word in words
+        for (String word : words) {
+            // init palindrome to true
+            boolean palindrome = true;
+            // init left to 0
+            int left = 0;
+            // init right to word length minus 1
+            int right = word.length() - 1;
+            // while left is less than right
+            while (left < right) {
+                // if word char at left is not equal to word char at right
+                if (word.charAt(left) != word.charAt(right)) {
+                     // set palindrome to false
+                    palindrome = false;
+                    // break
+                    break;
+                }
+                // increase left by 1
+                left++;
+                // decrease right by 1
+                right--;
+            }
+            // after while loop if palindrome is true
+            if (palindrome) {
+                // set ans to word
+                ans = word;
+                // break
+                break;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+    // main
+    public static void main(String[] args) {
+        String[] words = {"abc","car","ada","racecar","cool"};
+        System.out.println(findFirstPalindromicStringInTheArray(words));
+    }
 }
