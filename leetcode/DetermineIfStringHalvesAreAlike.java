7f830ef8249dba857c3e6d96ce10fc9c795a67af
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-09 21:05:03 -0700

    determine if string halves are alike using list

--- leetcode/DetermineIfStringHalvesAreAlike.java
+++ leetcode/DetermineIfStringHalvesAreAlike.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.List;
+
 /**
 
     You are given a string s of even length. Split this string into two halves of equal lengths, 
@@ -22,7 +24,43 @@ package leetcode;
 
 public class DetermineIfStringHalvesAreAlike {
     
-    // Time: O()
+    // Time: O(n)
     // determine if string halves are alike using list
-    
+    public static boolean determineIfStringHalvesAreAlike (String s) {
+        // init list of vowels
+        List<Character> list = List.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        // init a to 0
+        int a = 0;
+        // init b to 0
+        int b = 0;
+
+        // for each char i in s
+        for (int i = 0; i < s.length(); i++) {
+            // if i is less than s length divided by 2
+            if (i < s.length() / 2) {
+                // if list contains s char at i
+                if (list.contains(s.charAt(i))) {
+                    // increase a by 1
+                    a++;
+                } 
+            } else {
+                // else 
+                // if list contains s char at i
+                if (list.contains(s.charAt(i))) {
+                    // increase b by 1
+                    b++;
+                }
+            }
+        }
+
+        // return a equals b
+        return a == b;
+    }
+
+
+    // main
+    public static void main(String[] args) {
+        String s = "textbook";
+        System.out.println(determineIfStringHalvesAreAlike(s));
+    }
 }
