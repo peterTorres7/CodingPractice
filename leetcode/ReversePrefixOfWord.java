e551d60be04289412c395b265b3d2459d1657be0
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-10 22:04:18 -0700

    reverse prefix of word

--- leetcode/ReversePrefixOfWord.java
+++ leetcode/ReversePrefixOfWord.java
@@ -22,7 +22,39 @@ package leetcode;
 
 public class ReversePrefixOfWord {
    
-    // Time: O()
+    // Time: O(n)
     // reverse prefix of word 
-    
+    public static String reversePrefixOfWord (String word, char ch) {
+        // init wordChars to word to char array
+        char[] wordChars = word.toCharArray();
+        // init left to 0
+        int left = 0;
+        // init right to word index of ch
+        int right = word.indexOf(ch);
+
+        // while left is less than right
+        while (left < right) {
+            // init temp to wordChars at left
+            char temp = wordChars[left];
+            // set wordChars at left to wordChars at right
+            wordChars[left] = wordChars[right]; 
+            // set wordChars at right to tem
+            wordChars[right] = temp;
+            // increase left by 1
+            left++;
+            // decrease right by 1
+            right--;
+        }
+
+        // return new string of wordChars
+        return new String(wordChars);
+    }
+
+
+    // main
+    public static void main(String[] args) {
+        String word = "abcdefd";
+        char ch = 'd';
+        System.out.println(reversePrefixOfWord(word, ch));
+    }
 }
