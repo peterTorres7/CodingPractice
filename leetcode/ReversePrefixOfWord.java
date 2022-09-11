4f3cad0aeafa3c3dcaaf33c1387647534e32aa14
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-10 20:31:21 -0700

    created reverse prefix of word file and template

---
+++ leetcode/ReversePrefixOfWord.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+
+    Given a 0-indexed string word and a character ch, reverse the segment of word 
+    that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). 
+    If the character ch does not exist in word, do nothing.
+
+        For example, if word = "abcdefd" and ch = "d", then you should 
+        reverse the segment that starts at 0 and ends at 3 (inclusive). 
+        The resulting string will be "dcbaefd".
+
+    Return the resulting string.
+    
+
+    Example 1:
+
+    Input: word = "abcdefd", ch = "d"
+    Output: "dcbaefd"
+
+ */
+
+public class ReversePrefixOfWord {
+   
+    // Time: O()
+    // reverse prefix of word 
+    
+}
