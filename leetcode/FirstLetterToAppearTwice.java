b3fb1288f5281ea5f61418f29166abd4d50bcc43
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-13 12:14:13 -0700

    first letter to appear twice using map

--- leetcode/FirstLetterToAppearTwice.java
+++ leetcode/FirstLetterToAppearTwice.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
 
     Given a string s consisting of lowercase English letters, return the first letter to appear twice.
@@ -22,4 +25,33 @@ public class FirstLetterToAppearTwice {
     
     // Time: O(n)
     // first lettr to appear twice using map
+    public static char firstLetterToAppearTwiceMap (String s) {
+        // init ans to 0
+        char ans = 0;
+        // init map of Character, Integer
+        Map<Character, Integer> map = new HashMap<>();
+
+        // for each char in s to char array
+        for (char c : s.toCharArray()) {
+            // in map put char, map get or default char, 0 plus 1
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            // if map get char equals 2
+            if (map.get(c) == 2) {
+                // set ans to char
+                ans = c;
+                // break
+                break;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+    // main
+    public static void main(String[] args) {
+        String s = "abccbaacz";
+        System.out.println(firstLetterToAppearTwiceMap(s));
+    }
 }
