d606fc40a114fc3051439adf17b9efe740edc7a2
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-13 17:56:14 -0700

    counting words with a given prefix

--- leetcode/CountingWordsWithAGivenPrefix.java
+++ leetcode/CountingWordsWithAGivenPrefix.java
@@ -21,5 +21,28 @@ public class CountingWordsWithAGivenPrefix {
     
     // Time: O(n)
     // counting words with a given prefix
-    
+    public static int countingWordsWithAGivenPrefix (String[] words, String pref) {
+        // init ans to 0
+        int ans = 0;
+        
+        // for each word in words
+        for (String word : words) {
+            // if word index of pref equals 0
+            if (word.indexOf(pref) == 0) {
+                // increase ans by 1
+                ans++;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+    // main
+    public static void main(String[] args) {
+        String[] words = {"pay","attention","practice","attend"};
+        String pref = "at";
+        System.out.println(countingWordsWithAGivenPrefix(words, pref));
+    }
 }
