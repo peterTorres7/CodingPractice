d7eeb1a3b99da261db9ab7ec9011990deeca7d18
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-13 17:17:50 -0700

    created counting words with a given prefix file and template

---
+++ leetcode/CountingWordsWithAGivenPrefix.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+
+    You are given an array of strings words and a string pref.
+
+    Return the number of strings in words that contain pref as a prefix.
+
+    A prefix of a string s is any leading contiguous substring of s.
+
+    
+
+    Example 1:
+
+    Input: words = ["pay","attention","practice","attend"], pref = "at"
+    Output: 2
+
+ */
+
+public class CountingWordsWithAGivenPrefix {
+    
+    // Time: O(n)
+    // counting words with a given prefix
+    
+}
