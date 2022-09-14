661670701b85ce0822d70a758d1458afeabe689c
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-14 12:14:44 -0700

    created count prefixes of a given string file and template

---
+++ leetcode/CountPrefixesOfAGivenString.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+
+    You are given a string array words and a string s, 
+    where words[i] and s comprise only of lowercase English letters.
+
+    Return the number of strings in words that are a prefix of s.
+
+    A prefix of a string is a substring that occurs at the beginning of the string. 
+    A substring is a contiguous sequence of characters within a string.
+    
+
+    Example 1:
+
+    Input: words = ["a","b","c","ab","bc","abc"], s = "abc"
+    Output: 3
+
+ */
+
+public class CountPrefixesOfAGivenString {
+    
+    // Time: O(n)
+    // count prefixes of a given string
+    
+}
