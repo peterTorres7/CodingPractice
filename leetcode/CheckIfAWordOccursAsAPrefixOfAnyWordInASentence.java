73f75dd45d2a3dc24753edd9dd6527c06653d267
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-14 10:42:58 -0700

    created check if a word occurs as a prefix of any word in a sentence file and template

---
+++ leetcode/CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+
+    Given a sentence that consists of some words separated by a single space, and a searchWord, 
+    check if searchWord is a prefix of any word in sentence.
+
+    Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. 
+    If searchWord is a prefix of more than one word, return the index of the first word (minimum index). 
+    If there is no such word return -1.
+
+    A prefix of a string s is any leading contiguous substring of s.
+    
+
+    Example 1:
+
+    Input: sentence = "i love eating burger", searchWord = "burg"
+    Output: 4
+
+ */
+
+public class CheckIfAWordOccursAsAPrefixOfAnyWordInASentence {
+    
+    // Time: O(n)
+    // check if a word occurs as a prefix of any word in a sentence
+    
+}
