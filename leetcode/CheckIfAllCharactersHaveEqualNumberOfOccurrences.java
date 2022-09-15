bef0cbaa439fbc87e257ac6c2d86cace9f1dcd13
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-15 11:44:06 -0700

    check if all characters have equal number of occurrences using map and set

--- leetcode/CheckIfAllCharactersHaveEqualNumberOfOccurrences.java
+++ leetcode/CheckIfAllCharactersHaveEqualNumberOfOccurrences.java
@@ -1,5 +1,10 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
 
     Given a string s, return true if s is a good string, or false otherwise.
@@ -19,5 +24,27 @@ public class CheckIfAllCharactersHaveEqualNumberOfOccurrences {
     
     // Time: O(n)
     // check if all characters have equal number of occurrences using map and set
+    public static boolean checkIfAllCharactersHaveEqualNumberOfOccurrencesMapSet (String s) {
+        // init map of character, integer
+        Map<Character, Integer> map = new HashMap<>();
 
+        // for each char in s
+        for (char c : s.toCharArray()) {
+            // in map put char, map get or default c, 0 plus 1
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // init set of integer to map values
+        Set<Integer> set = new HashSet<>(map.values());
+
+        // return set size equals 1
+        return set.size() == 1;
+    }
+
+
+    // main 
+    public static void main(String[] args) {
+        String s = "abacbc";
+        System.out.println(checkIfAllCharactersHaveEqualNumberOfOccurrencesMapSet(s));
+    }
 }
