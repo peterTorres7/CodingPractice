647257a6f8ce3ed5cb12f3c3441f86f51fc920d8
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-15 10:36:15 -0700

    created check if all characters have equal number of occurrences using map and set file and template

---
+++ leetcode/CheckIfAllCharactersHaveEqualNumberOfOccurrences.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+
+    Given a string s, return true if s is a good string, or false otherwise.
+
+    A string s is good if all the characters that appear in s have the same number of occurrences 
+    (i.e., the same frequency).
+    
+
+    Example 1:
+
+    Input: s = "abacbc"
+    Output: true
+
+ */
+
+public class CheckIfAllCharactersHaveEqualNumberOfOccurrences {
+    
+    // Time: O(n)
+    // check if all characters have equal number of occurrences using map and set
+
+}
