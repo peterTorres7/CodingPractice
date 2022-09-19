4f4cfb067d8cd682f493ca0e0fe3094b6e7cdca6
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-19 09:21:22 -0700

    n repeated element in size 2n array using map

--- leetcode/NRepeatedElementInSize2NArray.java
+++ leetcode/NRepeatedElementInSize2NArray.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
 
     You are given an integer array nums with the following properties:
@@ -21,6 +24,35 @@ package leetcode;
 
 public class NRepeatedElementInSize2NArray {
     
-    // Time: O()
-    // N Repeated element in size 2N array
+    // Time: O(n)
+    // N Repeated element in size 2N array using map
+    public static int nRepeatedElementInSize2NArrayMap (int[] nums) {
+        // init map of integer, integer map to new hashmap
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // for each num in nums
+        for (int num : nums) {
+            // in map put num, map get or default num, 0 plus 1
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // for each num in nums
+        for (int num : nums) {
+            // if map get num equals nums length divided by 2
+            if (map.get(num) == nums.length / 2) {
+                // return num
+                return num;
+            }
+        }
+
+        // return -1
+        return -1;
+    }
+
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,3};
+        System.out.println(nRepeatedElementInSize2NArrayMap(nums));
+    }
 }
