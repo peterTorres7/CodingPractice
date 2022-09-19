bce979052cf3a245d0495f99b5a0fb7bf60eb0e9
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-19 08:34:56 -0700

    created n repeated element in size 2n array file and template

---
+++ leetcode/NRepeatedElementInSize2NArray.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+
+    You are given an integer array nums with the following properties:
+
+        nums.length == 2 * n.
+        nums contains n + 1 unique elements.
+        Exactly one element of nums is repeated n times.
+
+    Return the element that is repeated n times.
+
+    
+
+    Example 1:
+
+    Input: nums = [1,2,3,3]
+    Output: 3
+
+ */
+
+public class NRepeatedElementInSize2NArray {
+    
+    // Time: O()
+    // N Repeated element in size 2N array
+}
