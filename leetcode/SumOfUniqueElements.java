27da5d2fc531d87f3f4538b1a630bf116d55a5dd
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-19 13:10:10 -0700

    created sum of unique elements file and template

---
+++ leetcode/SumOfUniqueElements.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+
+    You are given an integer array nums. 
+    The unique elements of an array are the elements that appear exactly once in the array.
+
+    Return the sum of all the unique elements of nums.
+
+
+    Example 1:
+
+    Input: nums = [1,2,3,2]
+    Output: 4
+
+ */
+
+public class SumOfUniqueElements {
+    
+    // Time: O(n)
+    // sum of unique elements
+    
+}
