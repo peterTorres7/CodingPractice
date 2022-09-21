ff72eb50e762f367416b900c1d72a77c72e9e352
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-20 20:00:55 -0700

    created merge similar items file and template

---
+++ leetcode/MergeSimilarItems.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+
+    You are given two 2D integer arrays, items1 and items2, representing two sets of items. 
+    Each array items has the following properties:
+
+        items[i] = [valuei, weighti] where valuei represents the value and weighti represents the weight of the ith item.
+        The value of each item in items is unique.
+
+    Return a 2D integer array ret where ret[i] = [valuei, weighti], 
+    with weighti being the sum of weights of all items with value valuei.
+
+    Note: ret should be returned in ascending order by value.
+
+
+    Example 1:
+
+    Input: items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]
+    Output: [[1,6],[3,9],[4,5]]
+
+ */
+public class MergeSimilarItems {
+ 
+    // Time: O(n)
+    // merge similar items using map
+    
+}
