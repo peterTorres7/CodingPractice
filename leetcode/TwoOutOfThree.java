b99f7ebbd219378ce4482e1a8f409cb3713c5513
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-27 14:07:36 -0700

    created two out of three  file and template

---
+++ leetcode/TwoOutOfThree.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+
+    Given three integer arrays nums1, nums2, and nums3, 
+    return a distinct array containing all the values that are present in at least two out of the three arrays. 
+    You may return the values in any order.
+    
+
+    Example 1:
+
+    Input: nums1 = [1,1,3,2], nums2 = [2,3], nums3 = [3]
+    Output: [3,2]
+
+ */
+
+public class TwoOutOfThree {
+    
+    // Time: O(n)
+    // two out of three
+}
