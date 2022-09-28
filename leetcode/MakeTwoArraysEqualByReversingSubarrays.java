d20a7408fb4acc7f7160e20c0f690a4da29f3cee
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-28 12:03:59 -0700

    created make two arrays equal by reversing subarrays file and template

---
+++ leetcode/MakeTwoArraysEqualByReversingSubarrays.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+
+    You are given two integer arrays of equal length target and arr. 
+    In one step, you can select any non-empty subarray of arr and reverse it. 
+    You are allowed to make any number of steps.
+
+    Return true if you can make arr equal to target or false otherwise.
+
+    
+
+    Example 1:
+
+    Input: target = [1,2,3,4], arr = [2,4,1,3]
+    Output: true
+
+ */
+
+public class MakeTwoArraysEqualByReversingSubarrays {
+    // Time: O()
+    // make two arrays equal by reversing subarrays
+    
+}
