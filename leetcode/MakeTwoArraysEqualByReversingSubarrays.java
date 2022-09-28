8c880f171128772f3b28d4362c4be51afe149512
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-28 12:12:01 -0700

    make two arrays equal by reversing subarrays using sort

--- leetcode/MakeTwoArraysEqualByReversingSubarrays.java
+++ leetcode/MakeTwoArraysEqualByReversingSubarrays.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
 
     You are given two integer arrays of equal length target and arr. 
@@ -18,7 +20,36 @@ package leetcode;
  */
 
 public class MakeTwoArraysEqualByReversingSubarrays {
-    // Time: O()
+    // Time: O(n logn)
     // make two arrays equal by reversing subarrays
+    public static boolean makeTwoArraysEqualByReversingSubarrays (int[] target, int[] arr) {
+        // sort target
+        Arrays.sort(target);
+        // sort arr
+        Arrays.sort(arr);
+
+        // for each num i in target length
+        for (int i = 0; i < target.length; i++) {
+            // if target at i is not equal to arr at i
+            if (target[i] != arr[i]) {
+                // return false
+                return false;
+            }
+        }
+
+        // return true
+        return true;
+    }
+
+
     
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] target = {1,2,3,4}; 
+        int[] arr = {2,4,1,3};
+        System.out.println(makeTwoArraysEqualByReversingSubarrays(target, arr));
+    }
 }
