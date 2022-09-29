120b23c1d3c14269c43235ef2a5d5b2085913e36
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-29 13:44:04 -0700

    kth distinct string in an array using linked hashmap

--- leetcode/KthDistinctStringInAnArray.java
+++ leetcode/KthDistinctStringInAnArray.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
 
     A distinct string is a string that is present only once in an array.
@@ -20,7 +23,45 @@ package leetcode;
 
 public class KthDistinctStringInAnArray {
     
-    // Time: O()
+    // Time: O(n)
     // kth distinct string in an array
-    
+    public static String kthDistinctStringInAnArray (String[] arr, int k) {
+        // init map of string, integer
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+        // for each ar in arr
+        for (String ar : arr) {
+            // in map put ar, map get or default ar, 0 plus 1
+            map.put(ar, map.getOrDefault(ar, 0) + 1);
+        }
+        
+        // init distinct to 0
+        int distinct = 0;
+        // for each key in map key set
+        for (String key : map.keySet()) {
+            // if map get key equals 1
+            if (map.get(key) == 1) {
+                // increase dinstinct by 1
+                distinct++;
+                // if dinstinct equals k
+                if (distinct == k) {
+                    // return key
+                    return key;
+                }
+            }
+        }
+
+        // return ""
+        return "";
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        String[] arr = {"d","b","c","b","c","a"};
+        int k = 2;
+        System.out.println(kthDistinctStringInAnArray(arr, k));
+    }
+
 }
