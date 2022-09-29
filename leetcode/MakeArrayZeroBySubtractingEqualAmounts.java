3a7d7fdab1377354376b20aaf6f515e83bda7570
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-28 21:06:00 -0700

    make array zero by subtracting equal amounts using set

--- leetcode/MakeArrayZeroBySubtractingEqualAmounts.java
+++ leetcode/MakeArrayZeroBySubtractingEqualAmounts.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
 
     You are given a non-negative integer array nums. In one operation, you must:
@@ -19,7 +22,30 @@ package leetcode;
 
 public class MakeArrayZeroBySubtractingEqualAmounts {
     
-    // Time: O()
+    // Time: O(n)
     // make array zero by subtracting equal amounts
-    
+    public static int makeArrayZeroBySubtractingEqualAmounts (int[] nums) {
+        // init set of integer
+        Set<Integer> set = new HashSet<>();
+        
+        // for each num in nums
+        for (int num : nums) {
+            // if num is not equal to 0
+            if (num != 0) {
+                // to set add num
+                set.add(num);
+            }
+        }
+
+        // return set size
+        return set.size();
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {1,5,0,3,5};
+        System.out.println(makeArrayZeroBySubtractingEqualAmounts(nums));
+    }
 }
