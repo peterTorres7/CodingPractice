04f4cbe8c27137fbb2d79ef62b5ebf492115a3f2
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-28 20:52:24 -0700

    created make array zero by subtracting equal amounts file and template

---
+++ leetcode/MakeArrayZeroBySubtractingEqualAmounts.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+
+    You are given a non-negative integer array nums. In one operation, you must:
+
+        Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+        Subtract x from every positive element in nums.
+
+    Return the minimum number of operations to make every element in nums equal to 0.
+
+    
+    Example 1:
+
+    Input: nums = [1,5,0,3,5]
+    Output: 3
+
+ */
+
+public class MakeArrayZeroBySubtractingEqualAmounts {
+    
+    // Time: O()
+    // make array zero by subtracting equal amounts
+    
+}
