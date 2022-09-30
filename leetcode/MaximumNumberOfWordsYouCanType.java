a21e38329a34f9e81e47869018f7d6083b8bc5d4
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-30 11:58:32 -0700

    created maximum number of words you can type file and template

---
+++ leetcode/MaximumNumberOfWordsYouCanType.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+
+    There is a malfunctioning keyboard where some letter keys do not work. 
+    All other keys on the keyboard work properly.
+
+    Given a string text of words separated by a single space (no leading or trailing spaces) 
+    and a string brokenLetters of all distinct letter keys that are broken, 
+    return the number of words in text you can fully type using this keyboard.
+    
+
+    Example 1:
+
+    Input: text = "hello world", brokenLetters = "ad"
+    Output: 1
+
+ */
+
+public class MaximumNumberOfWordsYouCanType {
+    
+    // Time: O()
+    // maximum number of words you can type
+    
+}
