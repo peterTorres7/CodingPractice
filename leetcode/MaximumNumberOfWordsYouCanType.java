b049e6e969a4f0ee7dabf742ded4c55f507179bd
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-09-30 12:11:21 -0700

    maximum number of words you can type using brute force

--- leetcode/MaximumNumberOfWordsYouCanType.java
+++ leetcode/MaximumNumberOfWordsYouCanType.java
@@ -19,7 +19,43 @@ package leetcode;
 
 public class MaximumNumberOfWordsYouCanType {
     
-    // Time: O()
-    // maximum number of words you can type
-    
+    // Time: O(n*m)
+    // maximum number of words you can type using brute force
+    public static int maximumNumberOfWordsYouCanTypeBruteForce (String text, String brokenLetters) {
+        // init ans to 0
+        int ans = 0;
+        // init words to text split by " "
+        String[] words = text.split(" ");
+
+        // for each word in words
+        for (String word : words) {
+            // init valid to true
+            boolean valid = true;
+            // for each char in brokenLetters to charArray
+            for (char c : brokenLetters.toCharArray()) {
+                // if word index of char is not equal to -1
+                if (word.indexOf(c) != -1) {
+                    // set valid to false
+                    valid = false;
+                }
+            }
+            // after inner for loop if valid is true
+            if (valid) {
+                // increase ans by 1
+                ans++;
+            }
+        }
+
+        // return ans 
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        String text = "hello world";
+        String brokenLetters = "ad";
+        System.out.println(maximumNumberOfWordsYouCanTypeBruteForce(text, brokenLetters));
+    }
 }
