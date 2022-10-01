8712ec3e53f4e3ec341a8c587ae204048040d5fb
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-01 10:37:33 -0700

    substrings of size three with distinct characters using sliding window

--- leetcode/SubstringsOfSizeThreeWithDistinctCharacters.java
+++ leetcode/SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -6,10 +6,10 @@ package leetcode;
 
     Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
 
-    Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+    Note that if there are multiple occurrences of the same substring, 
+    every occurrence should be counted.
 
     A substring is a contiguous sequence of characters in a string.
-
     
 
     Example 1:
@@ -23,7 +23,47 @@ package leetcode;
 
 public class SubstringsOfSizeThreeWithDistinctCharacters {
  
-    // Time: O()
-    // substrings of size three with distinct characters
-    
+    // Time: O(n)
+    // substrings of size three with distinct characters using sliding window
+    public static int substringsOfSizeThreeWithDistinctCharacters (String s) {
+        // init ans to 0
+        int ans = 0;
+        
+        // init i to 0
+        int i = 0;
+        // init j to 1
+        int j = 1;
+        // init k to 2
+        int k = 2;
+
+        // while k is less than s length
+        while (k < s.length()) {
+            // if s char at i is not equal to s char at j
+            if (s.charAt(i) != s.charAt(j) 
+            // AND s char at i is not equal to s char at k
+            && s.charAt(i) != s.charAt(k)
+            // AND s char at j is not equal to s char at k
+            && s.charAt(j) != s.charAt(k)) {
+                // increase ans by 1
+                ans++; 
+            }
+            // increase i by 1
+            i++;
+            // increase j by 1
+            j++;
+            // increase k by 1
+            k++;
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        String s = "xyzzaz";
+        System.out.println(substringsOfSizeThreeWithDistinctCharacters(s));
+    }
 }
