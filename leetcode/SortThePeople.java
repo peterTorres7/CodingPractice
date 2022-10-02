84a691f03b2bd9d9f94a6c8f48a5db846a72640d
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-02 11:55:06 -0700

    sort the people using treemap

--- leetcode/SortThePeople.java
+++ leetcode/SortThePeople.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
 
     You are given an array of strings names, 
@@ -20,7 +24,41 @@ package leetcode;
 
 public class SortThePeople {
     
-    // Time: O()
-    // sort the people
-    
+    // Time: O(n)
+    // sort the people using map
+    public static String[] sortThePeople (String[] names, int[] heights) {
+        // init map of integer, string
+        Map<Integer, String> map = new TreeMap<>();
+        
+        // for each height i in heights length
+        for (int i = 0; i < heights.length; i++) {
+            // in map put heights at i names at i
+            map.put(heights[i], names[i]);
+        }
+
+        // init ans to map size
+        String[] ans = new String[map.size()];
+
+        // init index to ans length minus 1
+        int index = ans.length - 1;
+        // for each key in map key set
+        for (int key : map.keySet()) {
+            // set ans at index to map get key
+            ans[index] = map.get(key);
+            // decrease index by 1
+            index--;
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        String[] names = {"Mary","John","Emma"}; 
+        int[] heights = {180,165,170};
+        System.out.println(Arrays.toString(sortThePeople(names, heights)));
+    }
 }
