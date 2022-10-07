a72db6c0846164f14f8690278280a8baa246c4c8
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-07 10:20:55 -0700

    maximum product of two elements in an array using single for loop

--- leetcode/MaximumProductOfTwoElementsInAnArray.java
+++ leetcode/MaximumProductOfTwoElementsInAnArray.java
@@ -19,12 +19,40 @@ import java.util.Arrays;
 public class MaximumProductOfTwoElementsInAnArray {
     
     // Time: O(nlogn)
-    // maximum product of twor elements in an array
-    public static int maximumProductOfTwoElementsInAnArray (int[] nums) {
+    // maximum product of twor elements in an array using sort
+    public static int maximumProductOfTwoElementsInAnArraySort (int[] nums) {
         // sort nums
         Arrays.sort(nums);
         // return nums at nums length minus 1 minus 1 times nums at nums length minus 2 minus 1
         return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
+    }
+
+
+    // Time: O(n)
+    // maximum product of twor elements in an array
+    public static int maximumProductOfTwoElementsInAnArray (int[] nums) {
+        // init max to integer min value
+        int max = Integer.MIN_VALUE;
+        // init secondMax to integer min value
+        int secondMax = Integer.MIN_VALUE;
+
+        // for each num in nums
+        for (int num : nums) {
+            // if num is greater than max
+            if (num > max) {
+                // set secondMax to max
+                secondMax = max;
+                // set max to num
+                max = num;
+            // else if num is greater than secondMax
+            } else if (num > secondMax) {
+                // set secondMax to num
+                secondMax = num;
+            }
+        }
+
+        // return max minus 1 times secondMax minus 1
+        return (max - 1) * (secondMax - 1);
     }
 
 
