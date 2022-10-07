990ebb595b3495d12b89a1e48c5d6a8e4512efbe
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-07 09:52:40 -0700

    created maximum product of twor elements in an array file and template

---
+++ leetcode/MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+
+    Given the array of integers nums, you will choose two different indices i and j of that array. 
+    Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+    
+
+    Example 1:
+
+    Input: nums = [3,4,5,2]
+    Output: 12 
+
+ */
+
+public class MaximumProductOfTwoElementsInAnArray {
+    
+    // Time: O()
+    // maximum product of twor elements in an array
+    
+}
