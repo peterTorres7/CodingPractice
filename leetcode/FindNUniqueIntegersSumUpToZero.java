56eb9a4d7ad8a03c008b7ab7382da8610edd8df1
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-08 13:26:17 -0700

    created find n unique integers sum up to 0 file and template

---
+++ leetcode/FindNUniqueIntegersSumUpToZero.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+
+    Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+
+    Example 1:
+
+    Input: n = 5
+    Output: [-7,-1,1,3,4]
+    Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+ */
+
+public class FindNUniqueIntegersSumUpToZero {
+    
+    // Time:()
+    // find n unique integers sum up to 0
+    
+}
