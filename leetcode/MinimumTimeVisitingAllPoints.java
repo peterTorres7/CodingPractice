1132c6bd54dee134682284ffe18aeb6d70f23480
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-07 19:30:06 -0700

    minimum time visiting all points using single for loop

--- leetcode/MinimumTimeVisitingAllPoints.java
+++ leetcode/MinimumTimeVisitingAllPoints.java
@@ -28,7 +28,31 @@ package leetcode;
 
 public class MinimumTimeVisitingAllPoints {
     
-    // Time: O()
+    // Time: O(n)
     // minimum time visiting all points
-    
+    public static int minimumTimeVisitingAllPoints (int[][] points) {
+        // init time to 0
+        int time = 0;
+
+        // for each point i in points length minus 1
+        for (int i = 0; i < points.length - 1; i++) {
+            // init xDiff to math absolute of points at i, 0 minus points at i plus 1, 0
+            int xDiff = Math.abs(points[i][0] - points[i + 1][0]);
+            // init yDiff to math absolute of points at i, 1 minus points at i plus 1, 1
+            int yDiff = Math.abs(points[i][1] - points[i + 1][1]);
+            // to time add math max of xDiff, yDiff
+            time += Math.max(xDiff, yDiff);
+        }
+
+        // return time
+        return time;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[][] points = {{1,1},{3,4},{-1,0}};
+        System.out.println(minimumTimeVisitingAllPoints(points));
+    }
 }
