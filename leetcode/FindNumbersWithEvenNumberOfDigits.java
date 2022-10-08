f0fca22b37153fb93b2761ca932bb27273622793
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-08 14:59:48 -0700

    created find numbers with even number of digits file template

---
+++ leetcode/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+
+    Given an array nums of integers, return how many of them contain an even number of digits.
+
+    
+
+    Example 1:
+
+    Input: nums = [12,345,2,6,7896]
+    Output: 2
+
+ */
+
+public class FindNumbersWithEvenNumberOfDigits {
+    
+    // Time: O()
+    // find numbers with even number of digits
+    
+}
