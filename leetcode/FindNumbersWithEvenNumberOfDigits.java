185e10fe543df25f1cb0d2f7127b92ad3054f087
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-08 20:27:58 -0700

    find numbers with even number of digits using for and while loop

--- leetcode/FindNumbersWithEvenNumberOfDigits.java
+++ leetcode/FindNumbersWithEvenNumberOfDigits.java
@@ -15,7 +15,39 @@ package leetcode;
 
 public class FindNumbersWithEvenNumberOfDigits {
     
-    // Time: O()
+    // Time: O(nm)
     // find numbers with even number of digits
-    
+    public static int findNumbersWithEvenNumberOfDigits (int[] nums) {
+        // init ans to 0
+        int ans = 0;
+
+        // for each num in nums
+        for (int num : nums) {
+            // init length to 0
+            int length = 0;
+            // while num is greater than 0
+            while (num > 0) {
+                // set nun to divided by 10
+                num /= 10;
+                // increase length by 1
+                length++;
+            }
+            // after while loop if length is even 
+            if (length % 2 == 0) {
+                // increase ans by 1
+                ans++;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+        System.out.println(findNumbersWithEvenNumberOfDigits(nums));
+    }
 }
