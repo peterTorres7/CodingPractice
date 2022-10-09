d7948f14abf939600cb6088ee2c0d6d86e952377
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-09 12:55:55 -0700

    created find greatest common divisor of array file and template

---
+++ leetcode/FindGreatestCommonDivisorOfArray.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+
+    Given an integer array nums, 
+    return the greatest common divisor of the smallest number and largest number in nums.
+
+    The greatest common divisor of two numbers 
+    is the largest positive integer that evenly divides both numbers.
+    
+
+    Example 1:
+
+    Input: nums = [2,5,6,9,10]
+    Output: 2
+
+ */
+
+public class FindGreatestCommonDivisorOfArray {
+    
+    // Time: O()
+    // find greatest common divisor of array
+    
+}
