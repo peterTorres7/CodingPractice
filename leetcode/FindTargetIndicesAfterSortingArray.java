2ee3fab71ecaf2c9bbf186b2dc73279444a0befb
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-10 10:31:38 -0700

    find target indices after sorting array

--- leetcode/FindTargetIndicesAfterSortingArray.java
+++ leetcode/FindTargetIndicesAfterSortingArray.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
 
     You are given a 0-indexed integer array nums and a target element target.
@@ -22,7 +26,33 @@ package leetcode;
 
 public class FindTargetIndicesAfterSortingArray {
     
-    // Time: O()
+    // Time: O(nlogn)
     // find target indices after sorting array
-    
+    public static List<Integer> findTargetIndicesAfterSortingArray (int[] nums, int target) {
+        // init ans to list of integer
+        List<Integer> ans = new ArrayList<>();
+        // sort nums
+        Arrays.sort(nums);
+
+        // for each num i in nums length
+        for (int i = 0; i < nums.length; i++) {
+            // if nums at i is equal to target
+            if (nums[i] == target) {
+                // to ans add i
+                ans.add(i);
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {1,2,5,2,3};
+        int target = 2;
+        System.out.println(findTargetIndicesAfterSortingArray(nums, target));
+    }
 }
