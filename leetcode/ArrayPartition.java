23d48990a720d940923c9268cf1c06366852799c
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-10 11:31:32 -0700

    created array partition file and template

---
+++ leetcode/ArrayPartition.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/*
+
+    Given an integer array nums of 2n integers, 
+    group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) 
+    such that the sum of min(ai, bi) for all i is maximized. 
+    Return the maximized sum.
+    
+
+    Example 1:
+
+    Input: nums = [1,4,3,2]
+    Output: 4
+
+ */
+
+public class ArrayPartition {
+    
+    // Time: O()
+    // array partition
+    
+}
