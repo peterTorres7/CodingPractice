fa6ce64ac6ae34af759c09397daa0f676a54d79c
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-10 09:23:16 -0700

    find greatest common divisor of array using brute force

--- leetcode/FindGreatestCommonDivisorOfArray.java
+++ leetcode/FindGreatestCommonDivisorOfArray.java
@@ -18,7 +18,45 @@ package leetcode;
 
 public class FindGreatestCommonDivisorOfArray {
     
-    // Time: O()
-    // find greatest common divisor of array
-    
+    // Time: O(n)
+    // find greatest common divisor of array using brute force
+    public static int findGreatestCommonDivisorOfArrayBruteForce (int[] nums) {
+        // init min to integer max value
+        int min = Integer.MAX_VALUE;
+        // init max to integer min value
+        int max = Integer.MIN_VALUE;
+
+        // for each num in nums
+        for (int num : nums) {
+           // if num is less than min
+            if (num < min) {
+                // set min to num
+                min = num;
+            }
+            // if num is greater than max
+            if (num > max) {
+                // set max to num
+                max = num;
+            }
+        }
+
+        // init divisor to min
+        int divisor = min;
+        // while min modulo divisor is not equal to 0 OR max modulo divisor is not equal to 0
+        while (min % divisor != 0 || max % divisor != 0) {
+            // decrease divisor by 1
+            divisor--;
+        }
+
+        // return divisor
+        return divisor;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {2,5,6,9,10};
+        System.out.println(findGreatestCommonDivisorOfArrayBruteForce(nums));
+    }
 }
