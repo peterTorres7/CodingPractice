fa1febc90e263130b19702ac94693218ccb08818
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-10 11:41:48 -0700

    array partition using sort

--- leetcode/ArrayPartition.java
+++ leetcode/ArrayPartition.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /*
 
     Given an integer array nums of 2n integers, 
@@ -17,7 +19,29 @@ package leetcode;
 
 public class ArrayPartition {
     
-    // Time: O()
+    // Time: O(nlogn)
     // array partition
-    
+    public static int arrayPartition (int[] nums) {
+        // init ans to 0
+        int ans = 0;
+        // sort nums
+        Arrays.sort(nums);
+
+        // for each num i in nums length increasing i by 2
+        for (int i = 0; i < nums.length; i += 2) {
+            // to ans add nums at i
+            ans += nums[i];
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {1,4,3,2};
+        System.out.println(arrayPartition(nums));
+    }
 }
