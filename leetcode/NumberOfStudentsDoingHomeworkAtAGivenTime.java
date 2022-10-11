67c846ae2b30b2ab6232c7e29d16f62a644a1f85
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-11 13:01:16 -0700

    number of students doing homework at a given time using single for loop

--- leetcode/NumberOfStudentsDoingHomeworkAtAGivenTime.java
+++ leetcode/NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -19,7 +19,32 @@ package leetcode;
 
 public class NumberOfStudentsDoingHomeworkAtAGivenTime {
     
-    // Time: O()
+    // Time: O(n)
     // number of students doing homework at a given time
-    
+    public static int numberOfStudentsDoingHomeworkAtAGivenTime (int[] startTime, int[] endTime, int queryTime) {
+        // init ans to 0
+        int ans = 0;
+
+        // for each time i in startTime length
+        for (int i = 0; i < startTime.length; i++) {
+            // if startTime at i is greater than or equal to queryTime AND queryTime is less than or equal to endTime at i
+            if (startTime[i] <= queryTime && queryTime <= endTime[i]) {
+                // increase ans by 1
+                ans++;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] startTime = {1,2,3}; 
+        int[] endTime = {3,2,7}; 
+        int queryTime = 4;
+        System.out.println(numberOfStudentsDoingHomeworkAtAGivenTime(startTime, endTime, queryTime));
+    }
 }
