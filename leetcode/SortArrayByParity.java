564e4faef393dcc662fde0a6a8d69f4d8a9c8c67
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-11 15:06:28 -0700

    created sort array by parity file and template

---
+++ leetcode/SortArrayByParity.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+
+    Given an integer array nums, 
+    move all the even integers at the beginning of the array followed by all the odd integers.
+
+    Return any array that satisfies this condition.
+    
+
+    Example 1:
+
+    Input: nums = [3,1,2,4]
+    Output: [2,4,3,1]
+
+ */
+
+public class SortArrayByParity {
+
+    // Time: O()
+    // sort array by parity
+    
+}
