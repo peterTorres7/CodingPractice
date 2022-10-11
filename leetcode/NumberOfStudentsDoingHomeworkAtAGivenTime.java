5eb305ba0dd8b340e9682f563ce924e7e23ed749
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-11 12:03:41 -0700

    created number of students doing homework at a given time file and template

---
+++ leetcode/NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+
+    Given two integer arrays startTime and endTime and given an integer queryTime.
+
+    The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+
+    Return the number of students doing their homework at time queryTime. More formally, 
+    return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+    
+
+    Example 1:
+
+    Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+    Output: 1
+
+ */
+
+public class NumberOfStudentsDoingHomeworkAtAGivenTime {
+    
+    // Time: O()
+    // number of students doing homework at a given time
+    
+}
