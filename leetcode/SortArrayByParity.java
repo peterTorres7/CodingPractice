9c77ecdff099b81717fe6feaf0fb23e8bb8d81bf
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-11 17:47:29 -0700

    sort array by parity using two pointers

--- leetcode/SortArrayByParity.java
+++ leetcode/SortArrayByParity.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
 
     Given an integer array nums, 
@@ -17,7 +19,41 @@ package leetcode;
 
 public class SortArrayByParity {
 
-    // Time: O()
-    // sort array by parity
-    
+    // Time: O(n)
+    // sort array by parity using two pointers
+    public static int[] sortArrayByParity (int[] nums) {
+        // init ans to new int of nums length
+        int[] ans = new int[nums.length];
+        // init left to 0
+        int left = 0;
+        // init right to ans length minus 1
+        int right = ans.length - 1;
+
+        // for each num in nums
+        for (int num : nums) {
+            // if num is even
+            if (num % 2 == 0) {
+                // set ans at left to num
+                ans[left] = num;
+                // increase left by 1
+                left++;
+            } else {
+                // else set ans at right to num
+                ans[right] = num;
+                // decrease right by 1
+                right--;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,4};
+        System.out.println(Arrays.toString(sortArrayByParity(nums)));
+    }
 }
