5198f877074f63b4dda4ec013877356b726831c1
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-13 13:10:34 -0700

    created final prices with a special discount in a shop file and template

---
+++ leetcode/FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+
+    You are given an integer array prices where prices[i] is the price of the ith item in a shop.
+
+    There is a special discount for items in the shop. If you buy the ith item, 
+    then you will receive a discount equivalent to 
+    prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i]. 
+    Otherwise, you will not receive any discount at all.
+
+    Return an integer array answer where answer[i] is the final price you will pay for the ith item of the shop, 
+    considering the special discount.
+    
+
+    Example 1:
+
+    Input: prices = [8,4,6,2,3]
+    Output: [4,2,4,2,3]
+
+ */
+
+public class FinalPricesWithASpecialDiscountInAShop {
+    
+    // Time: O()
+    // final prices with a special discount in a shop
+    
+}
