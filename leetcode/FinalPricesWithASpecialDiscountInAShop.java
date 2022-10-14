d03cd32b25427f5e4123cc6e41ebfdb18f840078
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-13 19:24:53 -0700

    final prices with a special discount in a shop

--- leetcode/FinalPricesWithASpecialDiscountInAShop.java
+++ leetcode/FinalPricesWithASpecialDiscountInAShop.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
 
     You are given an integer array prices where prices[i] is the price of the ith item in a shop.
@@ -22,7 +24,41 @@ package leetcode;
 
 public class FinalPricesWithASpecialDiscountInAShop {
     
-    // Time: O()
+    // Time: O(nm)
     // final prices with a special discount in a shop
-    
+    public static int[] finalPricesWithASpecialDiscountInAShop (int[] prices) {
+        // init ans to new int array of prices length
+        int[] ans = new int[prices.length];
+        // init left to 0
+        int left = 0;
+
+        // while left is less than prices length
+        while (left < prices.length) {
+            // set ans at left to prices at left
+            ans[left] = prices[left];
+            // for each price i plus left to prices length
+            for (int i = left + 1; i < prices.length; i++) {
+                // if prices at left is greater or equal to prices at i
+                if (prices[left] >= prices[i]) {
+                    // set ans at left to prices at left minus prices at i
+                    ans[left] = prices[left] - prices[i];
+                    // break
+                    break;                        
+                }
+            }
+            // after for loop increase left by 1
+            left++;
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] prices = {8,4,6,2,3};
+        System.out.println(Arrays.toString(finalPricesWithASpecialDiscountInAShop(prices)));
+    }
 }
