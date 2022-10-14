3b2d4bbe0304ab4f7261b3ca619a8bbfd5e7c167
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-14 12:35:21 -0700

    created count negative numbers in a sorted matrix file and template

---
+++ leetcode/CountNegativeNumbersInASortedMatrix.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+
+    Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, 
+    return the number of negative numbers in grid.
+    
+
+    Example 1:
+
+    Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+    Output: 8
+
+ */
+
+public class CountNegativeNumbersInASortedMatrix {
+    
+    // Time: O()
+    // count negative numbers in a sorted matrix
+    
+}
