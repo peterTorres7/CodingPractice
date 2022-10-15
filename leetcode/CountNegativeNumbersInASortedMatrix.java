9eb082709297e0d3dbe0d7a73d6dd42476c50790
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-15 16:08:01 -0700

    count negative numbers in a sorted matrix

--- leetcode/CountNegativeNumbersInASortedMatrix.java
+++ leetcode/CountNegativeNumbersInASortedMatrix.java
@@ -15,7 +15,71 @@ package leetcode;
 
 public class CountNegativeNumbersInASortedMatrix {
     
-    // Time: O()
+    // Time: O(nm)
     // count negative numbers in a sorted matrix
-    
+    public static int countNegativeNumbersInASortedMatrix (int[][] grid) {
+        // init ans to 0
+        int ans = 0;
+
+
+        // for each row in grid
+        for (int[] row : grid) {
+            // init right to row length minus 1
+            int right = row.length - 1;
+            // while right is greater than or equal to 0 AND row at right is negative
+            while (right >= 0 && row[right] < 0) {
+                // increase ans by 1
+                ans++;
+                // decrease right by 1
+                right--;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+    // Time: O(n)
+    // count negative numbers in a sorted matrix using single loop
+    public static int countNegativeNumbersInASortedMatrixSingleLoop (int[][] grid) {
+        // init ans to 0
+        int ans = 0;
+        // init row to 0
+        int row = 0;
+        // init right to grid first row length minus 1
+        int right = grid[0].length - 1;
+
+        // while row is less than grid length
+        while (row < grid.length) {
+            // if grid at row, right is negative
+            if (grid[row][right] < 0) {
+                // increase ans by 1
+                ans++;
+                // decrease right by 1
+                right--;
+            } else {
+                // else decrease right by 1                
+                right--;
+            }
+            // if right is less than 0
+            if (right < 0) {
+                // increase row by 1
+                row++;
+                // set right to grid first row length minus 1 
+                right = grid[0].length - 1;                  
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[][] grid = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        System.out.println(countNegativeNumbersInASortedMatrixSingleLoop(grid));
+    }
 }
