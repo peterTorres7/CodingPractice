3de5af9dfd96f75ac9a9c18a63aa68943d3538ef
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-16 09:44:50 -0700

    created replace elements with greatest element on right side file and template

---
+++ leetcode/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+
+    Given an array arr, replace every element in that array 
+    with the greatest element among the elements to its right, and replace the last element with -1.
+
+    After doing so, return the array.
+
+    
+
+    Example 1:
+
+    Input: arr = [17,18,5,4,6,1]
+    Output: [18,6,6,6,1,-1]
+
+ */
+
+public class ReplaceElementsWithGreatestElementOnRightSide {
+
+    // Time: O()
+    // replace elements with greatest element on right side
+    
+}
