d58fbb828d1da4458a72a0771698b02be8f810da
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-19 18:26:11 -0700

    created maximum units on a truck file and template

---
+++ leetcode/MaximumUnitsOnATruck.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+
+    You are assigned to put some amount of boxes onto one truck. 
+    You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+        numberOfBoxesi is the number of boxes of type i.
+        numberOfUnitsPerBoxi is the number of units in each box of the type i.
+
+    You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. 
+    You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+    Return the maximum total number of units that can be put on the truck.
+    
+
+    Example 1:
+
+    Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+    Output: 8
+
+ */
+
+public class MaximumUnitsOnATruck {
+    
+    // Time: O()
+    // maximum units on a truck
+    
+}
