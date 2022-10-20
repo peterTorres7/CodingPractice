a261b7168ab2c243d56e9a76f9b6620669acd9ff
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-20 13:23:19 -0700

    maximum units on a truck using sort

--- leetcode/MaximumUnitsOnATruck.java
+++ leetcode/MaximumUnitsOnATruck.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
 
     You are assigned to put some amount of boxes onto one truck. 
@@ -23,7 +25,40 @@ package leetcode;
 
 public class MaximumUnitsOnATruck {
     
-    // Time: O()
-    // maximum units on a truck
-    
+    // Time: O(nlogn)
+    // maximum units on a truck using sort
+    public static int maximumUnitsOnATruckSort (int[][] boxTypes, int truckSize) {
+        // init ans to 0
+        int ans = 0;
+        // sort boxTypes based on capacity
+        Arrays.sort(boxTypes, (a, b) -> Integer.compare(b[1], a[1]));
+        
+        // for each boxType in boxTypes
+        for (int[] boxType : boxTypes) {
+            // if truckSize is greater than 0
+            if (truckSize > 0) {
+                // init possible to min of truckSize, boxType at 0
+                int possible = Math.min(truckSize, boxType[0]);
+                // increase ans by possible times boxType at 1
+                ans += possible * boxType[1];
+                // decrease truckSize by possible
+                truckSize -= possible;
+            } else {
+                // else return ans
+                return ans;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[][] boxTypes = {{1,3},{2,2},{3,1}};
+        int truckSize = 4;
+        System.out.println(maximumUnitsOnATruckSort(boxTypes, truckSize));
+    }
 }
