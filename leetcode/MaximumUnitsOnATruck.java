031ab297a04f702046bc2eed02db3a19fa1a028e
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-20 19:29:46 -0700

    maximum units on a truck using tree map

--- leetcode/MaximumUnitsOnATruck.java
+++ leetcode/MaximumUnitsOnATruck.java
@@ -1,6 +1,9 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
 
@@ -30,7 +33,7 @@ public class MaximumUnitsOnATruck {
     public static int maximumUnitsOnATruckSort (int[][] boxTypes, int truckSize) {
         // init ans to 0
         int ans = 0;
-        // sort boxTypes based on capacity
+        // sort boxTypes in reverse based on capacity
         Arrays.sort(boxTypes, (a, b) -> Integer.compare(b[1], a[1]));
         
         // for each boxType in boxTypes
@@ -54,11 +57,45 @@ public class MaximumUnitsOnATruck {
     }
 
 
+    // Time: O(n)
+    // maximum units on a truck using tree map
+    public static int maximumUnitsOnATruckMap (int[][] boxTypes, int truckSize) {
+        // init ans to 0
+        int ans = 0;
+        // init tree map in reverse order
+        Map<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
+
+        // for each boxType in boxTypes
+        for (int[] boxType : boxTypes) {
+            // in map put boxType at 0, map get or default boxType at 0, 0 plus boxType at 1
+            map.put(boxType[1], map.getOrDefault(boxType[1], 0) + boxType[0]);
+        }
+
+        // for each key in map keyset
+        for (int key : map.keySet()) {
+            // if truckSize is greater than 0
+            if (truckSize > 0) {
+                // init possible to min of map get key, truckSize
+                int possible = Math.min(map.get(key), truckSize);
+                // increase ans by possible times map get key
+                ans += possible * key;
+                // decrease truckSize by possible
+                truckSize -= possible;
+            } else {
+                // else return ans           
+                return ans;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
 
     // main
     public static void main(String[] args) {
         int[][] boxTypes = {{1,3},{2,2},{3,1}};
         int truckSize = 4;
-        System.out.println(maximumUnitsOnATruckSort(boxTypes, truckSize));
+        System.out.println(maximumUnitsOnATruckMap(boxTypes, truckSize));
     }
 }
