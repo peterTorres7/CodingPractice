995bdcd86383bf96747669592aa6d0a3540f46cc
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-22 14:16:20 -0700

    created the k weakest rows in a matrix file and template

---
+++ leetcode/TheKWeakestRowsInAMatrix.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+
+    You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). 
+    The soldiers are positioned in front of the civilians. 
+    That is, all the 1's will appear to the left of all the 0's in each row.
+
+    A row i is weaker than a row j if one of the following is true:
+
+        The number of soldiers in row i is less than the number of soldiers in row j.
+        Both rows have the same number of soldiers and i < j.
+
+    Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+
+    Example 1:
+
+    Input: mat = 
+    [[1,1,0,0,0],
+    [1,1,1,1,0],
+    [1,0,0,0,0],
+    [1,1,0,0,0],
+    [1,1,1,1,1]], 
+    k = 3
+    Output: [2,0,3]
+
+ */
+
+public class TheKWeakestRowsInAMatrix {
+    
+    // Time: O()
+    // the k weakest rows in a matrix
+    
+}
