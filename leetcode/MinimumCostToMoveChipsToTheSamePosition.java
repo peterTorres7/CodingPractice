466f6c0f4fab2e0d8948732c09366d7fac00f07f
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-23 15:12:56 -0700

    created minimum cost to move chips to the same position file and template

---
+++ leetcode/MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+
+    We have n chips, where the position of the ith chip is position[i].
+
+    We need to move all the chips to the same position. 
+    In one step, we can change the position of the ith chip from position[i] to:
+
+        position[i] + 2 or position[i] - 2 with cost = 0.
+        position[i] + 1 or position[i] - 1 with cost = 1.
+
+    Return the minimum cost needed to move all the chips to the same position.
+
+    
+
+    Example 1:
+
+    Input: position = [1,2,3]
+    Output: 1
+
+ */
+
+public class MinimumCostToMoveChipsToTheSamePosition {
+    
+    // Time: O()
+    // minimum cost to move chips to the same position
+}
