dfa16216b5511f227099f1e99afa2115ec8c5246
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-24 12:42:55 -0700

    reverse string in place

--- leetcode/ReverseString.java
+++ leetcode/ReverseString.java
@@ -17,7 +17,34 @@ package leetcode;
 
 public class ReverseString {
     
-    // Time: O()
+    // Time: O(n)
     // reverse string
-    
+    public static void reverseString (char[] s) {
+        // init left to 0
+        int left = 0;
+        // init right to s length minus 1
+        int right = s.length - 1;
+
+        // while left is less than right
+        while (left < right) {
+            // init temp to s at left
+            char temp = s[left];
+            // set s at left to s at right
+            s[left] = s[right];
+            // set s at right to temp
+            s[right] = temp;
+            // increase left by 1
+            left++;
+            // decrease right by 1
+            right--;
+        }
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+    }
 }
