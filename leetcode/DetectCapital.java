e6f8e761b1e675d5524a1c8379a34b09b3aeb8f3
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-26 13:35:51 -0700

    detect capital using string methods

--- leetcode/DetectCapital.java
+++ leetcode/DetectCapital.java
@@ -26,7 +26,24 @@ package leetcode;
 
 public class DetectCapital {
     
-    // Time: O()
+    // Time: O(n)
     // detect capital
-    
+    public static boolean detectCapital (String word) {
+        // if word equals word to upper case
+        // OR word equals word to lower case
+        // OR word equals word char at 0 to upper case plus word starting at 1 to lower case
+        // return true
+        // else return false
+        return word.equals(word.toUpperCase()) 
+            || word.equals(word.toLowerCase())
+            || word.equals(Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase());
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        String word = "FlaG";
+        System.out.println(detectCapital(word));
+    }
 }
