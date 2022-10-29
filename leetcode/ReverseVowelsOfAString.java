67488d41b6d056c8418f60ce46b09b802263001e
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-29 11:59:14 -0700

    reverse vowels of a string using two pointers

--- leetcode/ReverseVowelsOfAString.java
+++ leetcode/ReverseVowelsOfAString.java
@@ -17,7 +17,56 @@ package leetcode;
 
 public class ReverseVowelsOfAString {
     
-    // Time: O()
+    // Time: O(n)
     // reverse vowels of a string
-    
+    public static String reverseVowelsOfAString (String s) {
+        // init letters to s to char array
+        char[] letters = s.toCharArray();
+        // init left to 0
+        int left = 0;
+        // init right to letters length minus 1
+        int right = letters.length - 1;
+
+        // while left is less than right
+        while (left < right) {
+            // if letters at left is vowel AND letters at right is vowel
+            if (isVowel(letters[left]) && isVowel(letters[right])) {
+                // init temp to letters at left
+                char temp = letters[left];
+                // set letters at left to letters at right
+                letters[left] = letters[right];
+                // set letters at right to temp
+                letters[right] = temp;
+                // increase left by 1
+                left++;
+                // decrease right by 1
+                right--;
+            // else if letters at left is not vowel
+            } else if (!isVowel(letters[left])) {
+                // increase left by 1
+                left++;
+            } else {
+                // else decrease right by 1
+                right--;
+            }
+        }
+
+        // return new string of letters 
+        return new String(letters);
+    }
+
+    // helper method to check if char is vowel
+    public static boolean isVowel (Character c) {
+        // return if c is a vowel
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(reverseVowelsOfAString(s));
+    }
 }
