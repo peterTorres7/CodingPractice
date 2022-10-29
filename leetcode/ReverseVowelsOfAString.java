a8b710230d82841677ffa1a90da1b8b82d242aa4
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-29 11:27:47 -0700

    created reverse vowels of a string file and template

---
+++ leetcode/ReverseVowelsOfAString.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+
+    Given a string s, reverse only all the vowels in the string and return it.
+
+    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, 
+    more than once.
+    
+
+    Example 1:
+
+    Input: s = "hello"
+    Output: "holle"
+
+ */
+
+public class ReverseVowelsOfAString {
+    
+    // Time: O()
+    // reverse vowels of a string
+    
+}
