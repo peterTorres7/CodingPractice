8dadfbcafb27e5dac976198c7947e94c9d2e240a
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-10-31 11:15:22 -0700

    transpose matrix using nested loops

--- leetcode/TransposeMatrix.java
+++ leetcode/TransposeMatrix.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
 
     Given a 2D integer array matrix, return the transpose of matrix.
@@ -17,7 +19,34 @@ package leetcode;
 
 public class TransposeMatrix {
     
-    // Time: O()
+    // Time: O(n^2)
     // transpose matrix
-    
+    public static int[][] transposeMatrix (int[][] matrix) {
+        // init rows to matrix length
+        int rows = matrix.length;
+        // init cols to matrix at 0 length
+        int cols = matrix[0].length;
+        // init ans to new int of size cols, rows
+        int[][] ans = new int[cols][rows];
+
+        // for each i rows in matrix
+        for (int i = 0; i < rows; i++) {
+            // for each j cols in matrix
+            for (int j = 0; j < cols; j++) {
+                // set ans at j, i to matrix at i, j
+                ans[j][i] = matrix[i][j];
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Arrays.deepToString(transposeMatrix(matrix)));
+    }
 }
