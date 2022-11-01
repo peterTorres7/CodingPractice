4afeb552b9a17ac6a45f54572974ebf0185093cb
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-01 14:57:22 -0700

    valid anagram using sort

--- leetcode/ValidAnagram.java
+++ leetcode/ValidAnagram.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
 
     Given two strings s and t, return true if t is an anagram of s, and false otherwise.
@@ -17,6 +19,43 @@ package leetcode;
 
 public class ValidAnagram {
     
-    // Time: O()
-    // valid anagram
+    // Time: O(nlogn)
+    // valid anagram using sort
+    public static boolean validAnagramSort (String s, String t) {
+        // if s length is not equal to t length 
+        if (s.length() != t.length()) {
+            // return false
+            return false;
+        }
+
+        // init sLetters to s to char array
+        char[] sLetters = s.toCharArray();
+        // init tLetters to t to char array
+        char[] tLetters = t.toCharArray();
+        // sort sLetters
+        Arrays.sort(sLetters);
+        // sort tLetters
+        Arrays.sort(tLetters);
+
+        // for each i in sLetters
+        for (int i = 0; i < sLetters.length; i++) {
+            // if sLetters at i is not equal to tLetters at i
+            if (sLetters[i] != tLetters[i]) {
+                // return false
+                return false;
+            }
+        }
+
+        // return true
+        return true;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(validAnagramSort(s, t));
+    }
 }
