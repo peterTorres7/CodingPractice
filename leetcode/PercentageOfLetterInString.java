3097f2d36fdb94f146e095512b2dc8a72f14ad1f
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-04 16:01:22 -0700

    created percentage of letter in string file and template

---
+++ leetcode/PercentageOfLetterInString.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+
+    Given a string s and a character letter, 
+    return the percentage of characters in s that equal letter rounded down to the nearest whole percent.
+    
+
+    Example 1:
+
+    Input: s = "foobar", letter = "o"
+    Output: 33
+    Explanation:
+    The percentage of characters in s that equal the letter 'o' is 2 / 6 * 100% = 33% when rounded down, 
+    so we return 33.
+
+ */
+
+public class PercentageOfLetterInString {
+    
+    // Time: O()
+    // percentage of letter in string
+    
+}
