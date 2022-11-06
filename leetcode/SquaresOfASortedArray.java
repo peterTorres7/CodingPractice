7608588eb3a83990cf7f0b9302ec4d92883f4f5b
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-06 14:17:03 -0800

    squares of a sorted array using sort

--- leetcode/SquaresOfASortedArray.java
+++ leetcode/SquaresOfASortedArray.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
 
     Given an integer array nums sorted in non-decreasing order, 
@@ -18,7 +20,33 @@ package leetcode;
 
 public class SquaresOfASortedArray {
     
-    // Time: O()
+    // Time: O(nlogn)
     // squares of a sorted array
+    public static int[] squaresOfASortedArray (int[] nums) {
+        // init numsLength
+        int numsLength = nums.length;
+        // init ans to new int array of numLenght
+        int[] ans = new int[numsLength];
+
+        // for each i in nums
+        for (int i = 0; i < numsLength; i++) {
+            // set ans at i to nums at i times nums at i
+            ans[i] = nums[i] * nums[i];
+        }
+
+        // sort ans
+        Arrays.sort(ans);
+
+        // return ans
+        return ans;
+    }
+
+
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,3,10};
+        System.out.println(Arrays.toString(squaresOfASortedArray(nums)));
+    }
     
 }
