2cfd8f705e22f624a1799105ab4bdd3c111552ef
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-07 13:07:42 -0800

    created count common words with one occurrence file and template

---
+++ leetcode/CountCommonWordsWithOneOccurrence.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+    Given two string arrays words1 and words2, 
+    return the number of strings that appear exactly once in each of the two arrays.
+
+    
+
+    Example 1:
+
+    Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+    Output: 2
+
+ */
+
+public class CountCommonWordsWithOneOccurrence {
+    
+    // Time: O()
+    // count common words with one occurrence
+    
+}
