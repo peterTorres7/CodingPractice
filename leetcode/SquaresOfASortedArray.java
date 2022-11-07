fd765f4e4d4a4b1db885d3af261a32331fb9abcb
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-07 10:55:09 -0800

    squares of a sorted array using two pointers

--- leetcode/SquaresOfASortedArray.java
+++ leetcode/SquaresOfASortedArray.java
@@ -21,11 +21,11 @@ import java.util.Arrays;
 public class SquaresOfASortedArray {
     
     // Time: O(nlogn)
-    // squares of a sorted array
-    public static int[] squaresOfASortedArray (int[] nums) {
+    // squares of a sorted array using sort
+    public static int[] squaresOfASortedArraySort (int[] nums) {
         // init numsLength
         int numsLength = nums.length;
-        // init ans to new int array of numLenght
+        // init ans to new int array of numLength
         int[] ans = new int[numsLength];
 
         // for each i in nums
@@ -42,11 +42,48 @@ public class SquaresOfASortedArray {
     }
 
 
+    // Time: O(n)
+    // squares of a sorted arrays using two pointers
+    public static int[] squaresOfASortedArrayTwoPointers (int[] nums) {
+        // init numsLength
+        int numsLength = nums.length;
+        // init ans to new int array of numsLength
+        int[] ans = new int[numsLength];
+        // init left to 0
+        int left = 0;
+        // init right to numsLength minus 1
+        int right = numsLength - 1;
+
+        // for each i to 0
+        for (int i = numsLength - 1; i >= 0; i--) {
+            // init leftSquared to nums at left times nums at left
+            int leftSquared = nums[left] * nums[left];
+            // init rightSquared to nums at right times nums at right
+            int rightSquared = nums[right] * nums[right];
+            // if leftSquared is greater than rightSquared
+            if (leftSquared > rightSquared) {
+                // set ans at i to leftSquared
+                ans[i] = leftSquared;
+                // increase left by 1
+                left++;
+            } else {
+                // else set ans at i to rightSquared
+                ans[i] = rightSquared;
+                // decrease right by 1
+                right--;
+            }
+        }
+
+        // return ans
+        return ans;
+    }
+
+
 
     // main
     public static void main(String[] args) {
         int[] nums = {-4,-1,0,3,10};
-        System.out.println(Arrays.toString(squaresOfASortedArray(nums)));
+        System.out.println(Arrays.toString(squaresOfASortedArrayTwoPointers(nums)));
     }
     
 }
