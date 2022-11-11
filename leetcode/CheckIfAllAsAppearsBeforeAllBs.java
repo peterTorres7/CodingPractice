ffcea2f1c098e4543c136c8e7dbb3522c75f2dba
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-10 16:03:49 -0800

    created check if all a's appears before all b's file and template

---
+++ leetcode/CheckIfAllAsAppearsBeforeAllBs.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+
+    Given a string s consisting of only the characters 'a' and 'b', 
+    return true if every 'a' appears before every 'b' in the string. Otherwise, return false.
+    
+
+    Example 1:
+
+    Input: s = "aaabbb"
+    Output: true
+    Explanation:
+    The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5.
+    Hence, every 'a' appears before every 'b' and we return true.
+
+ */
+
+public class CheckIfAllAsAppearsBeforeAllBs {
+    
+    // Time: O()
+    // check if all a's appears before all b's
+    
+}
