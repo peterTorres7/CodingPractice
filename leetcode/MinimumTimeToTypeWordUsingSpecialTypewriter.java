b4813e5b32f3321954944af66d6540fe8f4e6e67
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-13 22:11:50 -0800

    created minimum time to type word using special typewriter file and template

---
+++ leetcode/MinimumTimeToTypeWordUsingSpecialTypewriter.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+
+    There is a special typewriter with lowercase English letters 'a' to 'z' arranged in a circle 
+    with a pointer. A character can only be typed if the pointer is pointing to that character. 
+    The pointer is initially pointing to the character 'a'.
+
+    Each second, you may perform one of the following operations:
+
+        Move the pointer one character counterclockwise or clockwise.
+        Type the character the pointer is currently on.
+
+    Given a string word, return the minimum number of seconds to type out the characters in word.
+    
+
+    Example 1:
+
+    Input: word = "abc"
+    Output: 5
+    Explanation: 
+    The characters are printed as follows:
+    - Type the character 'a' in 1 second since the pointer is initially on 'a'.
+    - Move the pointer clockwise to 'b' in 1 second.
+    - Type the character 'b' in 1 second.
+    - Move the pointer clockwise to 'c' in 1 second.
+    - Type the character 'c' in 1 second.
+
+ */
+
+public class MinimumTimeToTypeWordUsingSpecialTypewriter {
+    
+    // Time: O()
+    // minimum time to type word using special typewriter
+    
+}
