29164ad1e7089f869102273fb1d149abb62940f8
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-19 10:43:30 -0800

    intersection of multiple arrays using map

--- leetcode/IntersectionOfMultipleArrays.java
+++ leetcode/IntersectionOfMultipleArrays.java
@@ -1,7 +1,10 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
 
@@ -24,20 +27,40 @@ public class IntersectionOfMultipleArrays {
         // init list
         List<Integer> list = new ArrayList<>();
         // init map
-        // init nums length
+        Map<Integer, Integer> map = new HashMap<>();
+        // init numsLength
+        int numsLength = nums.length;
 
         // for each num array in nums
-        // for each num in num array
-        // in put num and increase frequency by 1
+        for (int[] numArray : nums) {
+            // for each num in num array
+            for (int num : numArray) {
+                // in map put num and increase frequency by 1
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
 
         // for each num in nums at 0
-        // if num is equal to map get num
-        // to list add num
+        for (int num : nums[0]) {
+            // if numsLength is equal to map get num
+            if (numsLength == map.get(num)) {
+                // to list add num
+                list.add(num);
+            }
+        }
 
         // sort list
-
+        Collections.sort(list);
+        
         // return list
         return list;
+    }
 
+
+
+    // main
+    public static void main(String[] args) {
+        int[][] nums = {{3,1,2,4,5},{1,2,3,4},{3,4,5,6}};
+        System.out.println(intersectionOfMultipleArrays(nums));
     }
 }
