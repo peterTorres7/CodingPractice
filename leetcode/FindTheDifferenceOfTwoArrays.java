010c1fd884075b66281f4780ad9a25c1cc71fb5b
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-21 11:54:58 -0800

    created find the difference of two arrays file and template

---
+++ leetcode/FindTheDifferenceOfTwoArrays.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+
+    Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+        answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+        answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+
+    Note that the integers in the lists may be returned in any order.
+
+    
+
+    Example 1:
+
+    Input: nums1 = [1,2,3], nums2 = [2,4,6]
+    Output: [[1,3],[4,6]]
+
+ */
+
+public class FindTheDifferenceOfTwoArrays {
+    
+    // Time: O()
+    // find the difference of two arrays
+    
+}
