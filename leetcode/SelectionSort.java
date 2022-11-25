23ba91e23bb3aa0f255a8319373d3a0fa033e744
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-25 11:27:35 -0800

    redid selection sort pseudo code

--- leetcode/SelectionSort.java
+++ leetcode/SelectionSort.java
@@ -9,14 +9,16 @@ public class SelectionSort {
     // Time: O(n^2)
     // selection sort
     public static int[] selectionSort (int[] nums) {
-        // for each num i in nums
+        // init numsLength
+
+        // for each num i in numsLength mimus 1
         // init minIndex to i
-        // for each num j in nums
-        // if nums at j is less than nums at i
+        // for each j set to i plus 1 in numsLength
+        // if nums at j is less than nums at minIndex
         // set minIndex to j
-        // after inner for loop set temp to nums at i
-        // set nums at i to nums at j
-        // set nums at j to temp
+        // after inner loop set temp to nums at minIndex
+        // set nums at minIndex to nums at i
+        // set nums at i to temp
 
         // return nums
     }
