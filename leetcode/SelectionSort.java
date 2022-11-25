76bf6e08accc777a42a82053510cb82167f21a9e
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2022-11-25 09:55:52 -0800

    added selection sort pseudo code

--- leetcode/SelectionSort.java
+++ leetcode/SelectionSort.java
@@ -6,7 +6,18 @@ package leetcode;
 
 public class SelectionSort {
     
-    // Time: O()
+    // Time: O(n^2)
     // selection sort
-    
+    public static int[] selectionSort (int[] nums) {
+        // for each num i in nums
+        // init minIndex to i
+        // for each num j in nums
+        // if nums at j is less than nums at i
+        // set minIndex to j
+        // after inner for loop set temp to nums at i
+        // set nums at i to nums at j
+        // set nums at j to temp
+
+        // return nums
+    }
 }
