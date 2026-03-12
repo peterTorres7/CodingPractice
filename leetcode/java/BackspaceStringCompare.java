f7dd78fee34db0c527dbeeb8d3fede230c7d1ea0
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2026-03-12 01:57:03 -0400

    backspace string compare in java using two pointers

--- leetcode/java/BackspaceStringCompare.java
+++ leetcode/java/BackspaceStringCompare.java
@@ -12,7 +12,78 @@ import java.util.Stack;
 // Output: true
 
 public class BackspaceStringCompare {
-    public static boolean backspaceStringCompare(String s, String t) {
+
+    // pattern: two pointers
+    public static boolean BackspaceStringComparePointers(String s, String t) {
+        // init pointers and counters
+        int sPointer = s.length() - 1;
+        int tPointer = t.length() - 1;
+        int sSkip = 0;
+        int tSkip = 0;
+
+        // while t or s have letters to read
+        while (sPointer >= 0 || tPointer >= 0) {
+            // while s has letters to read
+            while (sPointer >= 0) {
+                // if letter is '#' increase s skip and decrease s pointer
+                if (s.charAt(sPointer) == '#') {
+                    sSkip++;
+                    sPointer--;
+                }
+                // else if there are s skip decrease s pointer and s skip
+                else if (sSkip > 0) {
+                    sSkip--;
+                    sPointer--;
+                }
+                // else break since there is s letter to compare   
+                else break;            
+            }
+
+            // while t has letters to read
+            while (tPointer >= 0) {
+                // if letter is '#' increase t skip and decrease t pointer
+                if (t.charAt(tPointer) == '#') {
+                    tSkip++;
+                    tPointer--;
+                }
+                // else if there are t skip decrease t pointer and t skip
+                else if (tSkip > 0) {
+                    tSkip--;
+                    tPointer--;
+                }
+                // else break since there is t letter to compare    
+                else break;            
+            }
+
+            
+            // if s and t have letters to read
+            if (sPointer >= 0 && tPointer >= 0) {
+                // if letters don't match return false
+                if (s.charAt(sPointer) != t.charAt(tPointer)) {
+                    return false;
+                }
+            }
+            // else if s has letters to read and t does not or s doesn't have letters to read and t does return false
+            else if ((sPointer >= 0 && tPointer < 0) || (sPointer < 0 && tPointer >= 0)) {
+                return false;
+            }
+
+            // decrease t and s pointer since letters match            
+            sPointer--;
+            tPointer--;
+        }
+
+        
+        // return true since s and t match
+        return true;
+    }
+
+    // time: O(n + m)
+    // space: O(1)
+
+
+    // pattern: stacks
+    public static boolean backspaceStringCompareStacks(String s, String t) {
             // init stacks
             Stack<Character> sStack = new Stack<>();
             Stack<Character> tStack = new Stack<>();
@@ -55,14 +126,17 @@ public class BackspaceStringCompare {
             return sStack.equals(tStack);
     }
 
+    // time: O(n + m)
+    // space: O(n)
+
+
     // main
     public static void main(String[] args) {
         String s = "ab#c";
         String t = "ad#c";
 
-        System.out.println(backspaceStringCompare(s, t));
+        System.out.println(backspaceStringCompareStacks(s, t));
+        System.out.println(BackspaceStringComparePointers(s, t));
     }
 }
 
-// time: O(n)
-// space: O(n)
