4e1e5b8ead2401c2c8ac63b127067402df090eb2
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2026-04-25 18:56:05 -0400

    valid palindrome using two pointers in java

--- leetcode/java/ValidPalindrome.java
+++ leetcode/java/ValidPalindrome.java
@@ -1,69 +1,53 @@
-package leetcode;
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters 
+// and removing all non-alphanumeric characters, it reads the same forward and backward. 
+// Alphanumeric characters include letters and numbers.
 
-/**
-
-    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters 
-    and removing all non-alphanumeric characters, it reads the same forward and backward. 
-    Alphanumeric characters include letters and numbers.
-
-    Given a string s, return true if it is a palindrome, or false otherwise.
+// Given a string s, return true if it is a palindrome, or false otherwise.
     
 
-    Example 1:
+// Example 1:
 
-    Input: s = "A man, a plan, a canal: Panama"
-    Output: true
-
- */
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
 
 public class ValidPalindrome {
-    
-    // Time: O(n)
-    // valid palindrome
-    public static boolean validPalindrome (String s) {
-        // init stringbuilder sb
-        StringBuilder sb = new StringBuilder();
-        // init sLower to s to lower case
-        String sLower = s.toLowerCase();
-
-        // for each char in sLower
-        for (char c : sLower.toCharArray()) {
-            // if char is between 'a' to 'z' OR '0' to '9'
-            if (c >= 'a' && c <= 'z' || c - '0' >= 0 && c - '0' <= 9) {
-                // to sb append char
-                sb.append(c);
-            }
-        }
-
-        // init letters to sb to string to char array
-        char[] letters = sb.toString().toCharArray();
-        // init left to 0
+    public static boolean isPalindrome(String s) {
+        // init pointers at ends
         int left = 0;
-        // init right to letters length minus 1
-        int right = letters.length - 1;
+        int right = s.length() - 1;
 
-        // while left is less than right
+        // while pointers don't cross
         while (left < right) {
-            // if letters at left is not equal to letters at right
-            if (letters[left] != letters[right]) {
-                // return false
-                return false;
+            // confirm while pointers don't cross and left is not letter or digit increase left by 1
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
             }
-            // increase left by 1
+            // confirm while pointers don't cross and right is not letter or digit decrease right by 1
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // if lowercase left is not equal to lowercase right return false
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false; 
+            }
+
+            // increase left and decrease right by 1
             left++;
-            // decrease right by 1
             right--;
         }
 
-        // return true
+        // return true since they all match
         return true;
     }
-
 
 
     // main
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-        System.out.println(validPalindrome(s));
+        System.out.println(isPalindrome(s));
     }
 }
+
+// time: O(n)
+// space: O(1)
